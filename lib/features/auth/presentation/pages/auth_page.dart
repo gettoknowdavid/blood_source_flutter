cd64ef656e54d40cc2f2e56d8e3380aa5973b723
David Michael II <knowdavidmichael@gmail.com>
2022-05-06 21:08:13 +0100

    feat: complete sign in page

--- lib/features/auth/presentation/pages/auth_page.dart
+++ lib/features/auth/presentation/pages/auth_page.dart
@@ -1,15 +1,186 @@
+import 'package:blood_source/common/app_colors.dart';
+import 'package:blood_source/common/header_painter.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AuthPage extends StatefulWidget {
-	@override
-	createState() => _AuthPage();
+  const AuthPage({Key? key}) : super(key: key);
+
+  @override
+  createState() => _AuthPage();
 }
+
 class _AuthPage extends State<AuthPage> {
-	@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-			appBar: AppBar(),
-		);
-	}
+  bool _isObscure = true;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: AppColors.primary.withOpacity(0.02),
+          child: CustomPaint(
+            painter: HeaderPainter(),
+            child: Center(
+              child: SingleChildScrollView(
+                padding: EdgeInsets.symmetric(horizontal: 24.r),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      'Hello Again!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 32.sp,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                    4.verticalSpace,
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 40.r),
+                      child: Text(
+                        "Welcome back, you've been missed!",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 18.sp,
+                          color: Colors.white70,
+                        ),
+                      ),
+                    ),
+                    40.verticalSpace,
+                    TextField(
+                      style: TextStyle(fontSize: 18.sp),
+                      decoration: InputDecoration(
+                        hintText: 'Enter email',
+                        fillColor: Colors.white,
+                        filled: true,
+                        contentPadding: EdgeInsets.symmetric(
+                          vertical: 24.r,
+                          horizontal: 26.r,
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(12.r),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: AppColors.primary,
+                          ),
+                          borderRadius: BorderRadius.circular(12.r),
+                        ),
+                      ),
+                    ),
+                    18.verticalSpace,
+                    TextField(
+                      obscureText: _isObscure,
+                      style: TextStyle(fontSize: 18.sp),
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        fillColor: Colors.white,
+                        filled: true,
+                        suffixIcon: IconButton(
+                          icon: _isObscure
+                              ? const Icon(Icons.visibility)
+                              : const Icon(Icons.visibility_off),
+                          padding: EdgeInsets.all(26.r),
+                          onPressed: () {
+                            setState(() => _isObscure = !_isObscure);
+                          },
+                        ),
+                        contentPadding: EdgeInsets.symmetric(
+                          vertical: 24.r,
+                          horizontal: 26.r,
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(12.r),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: AppColors.primary,
+                          ),
+                          borderRadius: BorderRadius.circular(12.r),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.zero,
+                        alignment: Alignment.centerRight,
+                        splashFactory: NoSplash.splashFactory,
+                      ),
+                      child: Text(
+                        'Password Recovery',
+                        style: TextStyle(
+                          fontSize: 16.sp,
+                          color: Colors.black54,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    40.verticalSpace,
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        primary: AppColors.swatch.shade700,
+                        padding: EdgeInsets.all(23.r),
+                        shadowColor: AppColors.swatch.shade400,
+                        elevation: 20,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.r),
+                        ),
+                      ),
+                      child: Text(
+                        'Sign In',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.5.sp,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    30.verticalSpace,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Not a member?',
+                          style: TextStyle(
+                            fontSize: 17.sp,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black54,
+                          ),
+                        ),
+                        6.horizontalSpace,
+                        TextButton(
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            padding: EdgeInsets.zero,
+                            splashFactory: NoSplash.splashFactory,
+                          ),
+                          child: Text(
+                            'Register now',
+                            style: TextStyle(
+                              fontSize: 17.sp,
+                              fontWeight: FontWeight.w600,
+                              color: AppColors.swatch.shade500,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
