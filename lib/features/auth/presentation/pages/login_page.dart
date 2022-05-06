5de0038307c7a2c835fa2bfd06096bcaa63596f1
David Michael II <knowdavidmichael@gmail.com>
2022-05-06 22:13:36 +0100

    refactor: Email and Password text fields

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -2,6 +2,7 @@ import 'package:blood_source/common/app_colors.dart';
 import 'package:blood_source/common/header_painter.dart';
 import 'package:blood_source/shared/widgets/app_button.dart';
 import 'package:blood_source/shared/widgets/app_text_button.dart';
+import 'package:blood_source/shared/widgets/app_textfield.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -70,62 +71,16 @@ class _LoginPage extends State<LoginPage> {
                       ),
                     ),
                     40.verticalSpace,
-                    TextField(
+                    AppTextField(
                       controller: _emailController,
-                      style: TextStyle(fontSize: 18.sp),
-                      decoration: InputDecoration(
-                        hintText: 'Enter email',
-                        fillColor: Colors.white,
-                        filled: true,
-                        contentPadding: EdgeInsets.symmetric(
-                          vertical: 24.r,
-                          horizontal: 26.r,
-                        ),
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius: BorderRadius.circular(12.r),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            color: AppColors.primary,
-                          ),
-                          borderRadius: BorderRadius.circular(12.r),
-                        ),
-                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      hintText: 'Enter email',
                     ),
                     18.verticalSpace,
-                    TextField(
+                    AppTextField(
                       controller: _passwordController,
-                      obscureText: _isObscure,
-                      style: TextStyle(fontSize: 18.sp),
-                      decoration: InputDecoration(
-                        hintText: 'Password',
-                        fillColor: Colors.white,
-                        filled: true,
-                        suffixIcon: IconButton(
-                          icon: _isObscure
-                              ? const Icon(Icons.visibility)
-                              : const Icon(Icons.visibility_off),
-                          padding: EdgeInsets.all(26.r),
-                          onPressed: () {
-                            setState(() => _isObscure = !_isObscure);
-                          },
-                        ),
-                        contentPadding: EdgeInsets.symmetric(
-                          vertical: 24.r,
-                          horizontal: 26.r,
-                        ),
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius: BorderRadius.circular(12.r),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            color: AppColors.primary,
-                          ),
-                          borderRadius: BorderRadius.circular(12.r),
-                        ),
-                      ),
+                      hintText: 'Password',
+                      isPassword: true,
                     ),
                     AppTextButton(
                       text: 'Password Recovery',
