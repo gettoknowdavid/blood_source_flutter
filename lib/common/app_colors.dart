dc8bcfb226e18529ec59d2e824dd350111bf56d8
David Michael II <knowdavidmichael@gmail.com>
2022-05-06 16:21:02 +0100

    feat: add app colors

--- lib/common/app_colors.dart
+++ lib/common/app_colors.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-	static final Color baseColor = Color(0xFF212735);
+  static const primary = Color(0xFFD7212D);
+  static const primaryDark = Color(0xFFB21628);
+  static const accent = Color(0xFFf48C91);
+  static const background = Color(0xFFFDFDFD);
+  static const swatch = MaterialColor(0xffD7212D, {
+    50: Color.fromRGBO(215, 33, 45, .1),
+    100: Color.fromRGBO(215, 33, 45, .2),
+    200: Color.fromRGBO(215, 33, 45, .3),
+    300: Color.fromRGBO(215, 33, 45, .4),
+    400: Color.fromRGBO(215, 33, 45, .5),
+    500: Color.fromRGBO(215, 33, 45, .6),
+    600: Color.fromRGBO(215, 33, 45, .7),
+    700: Color.fromRGBO(215, 33, 45, .8),
+    800: Color.fromRGBO(215, 33, 45, .9),
+    900: Color.fromRGBO(215, 33, 45, 1),
+  });
 }
