21a11dacc41636ac127e515d1a233ea256a3c697
David Michael II <knowdavidmichael@gmail.com>
2022-05-06 16:52:52 +0100

    feat: complete custom Theme

--- lib/common/app_themes.dart
+++ lib/common/app_themes.dart
@@ -1,3 +1,18 @@
-class AppThemes {
-	//static final ThemeData appTheme = ThemeData()
+import 'package:blood_source/common/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final ThemeData appTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    primaryColorDark: AppColors.primaryDark,
+    fontFamily: 'Poppins',
+    primarySwatch: AppColors.swatch,
+    colorScheme: const ColorScheme.light().copyWith(
+      primary: AppColors.primary,
+      onPrimary: Colors.white,
+      secondary: AppColors.secondary,
+      onSecondary: Colors.black87,
+      background: AppColors.background,
+    ),
+  );
 }
