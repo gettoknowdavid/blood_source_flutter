745ccd3572c600d92fd65c2f261f0fde3ed8d99a
David Michael II <knowdavidmichael@gmail.com>
2022-05-06 17:28:59 +0100

    feat: complete splash screen

--- lib/features/theming/presentation/pages/splash_page.dart
+++ lib/features/theming/presentation/pages/splash_page.dart
@@ -1,13 +1,24 @@
+import 'package:blood_source/common/image_resources.dart';
 import 'package:flutter/material.dart';
 
-
 class SplashPage extends StatelessWidget {
-  const SplashPage({ Key? key }) : super(key: key);
+  const SplashPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.width * 0.95,
+            width: MediaQuery.of(context).size.width * 0.95,
+            child: Image.asset(
+              ImageResources.logo,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
