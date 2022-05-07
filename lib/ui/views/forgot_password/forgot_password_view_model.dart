c568d25ac98db846fdacb4d017a6a4ed57402621
David Michael II <knowdavidmichael@gmail.com>
2022-05-08 00:10:42 +0100

    feat: initialize fogot password model

--- lib/ui/views/forgot_password/forgot_password_view_model.dart
+++ lib/ui/views/forgot_password/forgot_password_view_model.dart
@@ -1,5 +1,37 @@
+import 'package:blood_source/app/app.locator.dart';
+import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_firebase_auth/stacked_firebase_auth.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 class ForgotPasswordViewModel extends BaseViewModel with ReactiveServiceMixin {
   Future<void> init() async {}
+
+  NavigationService navigationService = locator<NavigationService>();
+  FirebaseAuthenticationService authService =
+      locator<FirebaseAuthenticationService>();
+
+  final GlobalKey<FormState> _signInFormKey = GlobalKey<FormState>();
+  GlobalKey<FormState> get signInFormKey => _signInFormKey;
+
+  final TextEditingController emailController = TextEditingController();
+
+  bool isFormValidated() {
+    if (emailController.text.isEmpty ||
+        !RegExp(r'\S+@\S+\.\S+').hasMatch(emailController.text)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  void initialise() {
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    super.dispose();
+  }
 }
