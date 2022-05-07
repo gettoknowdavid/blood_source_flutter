e2145cab4057c5adac3f6205470e1550d83f0d4a
David Michael II <knowdavidmichael@gmail.com>
2022-05-07 03:57:43 +0100

    feat: update app_back_button

--- lib/ui/shared/widgets/app_back_button.dart
+++ lib/ui/shared/widgets/app_back_button.dart
@@ -1,17 +1,20 @@
+import 'package:blood_source/app/app.locator.dart';
 import 'package:blood_source/common/image_resources.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 class AppBackButton extends StatelessWidget {
   const AppBackButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    NavigationService navService = locator<NavigationService>();
+
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 24.r, vertical: 20.r),
       child: GestureDetector(
-        onTap: () => Get.back(),
+        onTap: () => navService.back(),
         child: const ImageIcon(
           AssetImage(ImageResources.letArrow),
           color: Colors.white,
