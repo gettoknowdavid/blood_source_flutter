cdd0140a1a80f17c19c24832d3a581d0f49ac75c
David Michael II <knowdavidmichael@gmail.com>
2022-05-07 23:46:33 +0100

    feat: add forgot-email image

--- lib/common/image_resources.dart
+++ lib/common/image_resources.dart
@@ -2,4 +2,5 @@ class ImageResources {
   static const String logo = "assets/images/logo-splash.png";
   static const String leftArrow = "assets/images/left-arrow.png";
   static const String verifyEmail = "assets/images/verify-email.png";
+  static const String forgotPassword = "assets/images/forgot-email.png";
 }
