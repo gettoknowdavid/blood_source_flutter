ccd5b7618ff67b4948324c4d560d7f46b9ca3e36
David Michael II <knowdavidmichael@gmail.com>
2022-05-07 23:21:25 +0100

    fix: clear unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:blood_source/app/app.locator.dart';
 import 'package:blood_source/app/app.router.dart';
 import 'package:blood_source/common/app_themes.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -31,88 +30,6 @@ class MyApp extends StatelessWidget {
         theme: AppTheme.appTheme,
         navigatorKey: StackedService.navigatorKey,
         onGenerateRoute: StackedRouter().onGenerateRoute,
-      ),
-    );
-  }
-}
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return ScreenUtilInit(
-//       designSize: const Size(393, 830),
-//       minTextAdapt: true,
-//       splitScreenMode: true,
-//       child: GetMaterialApp(
-//         title: 'Flutter Demo',
-//         theme: AppTheme.appTheme,
-//         initialRoute: '/splash',
-//         defaultTransition: Transition.fadeIn,
-//         getPages: [
-//           GetPage(name: '/splash', page: () => const SplashPage()),
-//           GetPage(name: '/signIn', page: () => const SignInPage()),
-//           GetPage(name: '/signUp', page: () => const SignUpPage()),
-//           GetPage(name: '/', page: () => const MyHomePage(title: 'BS')),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'BloodSource',
-              style: Theme.of(context).textTheme.displayMedium,
-            ),
-            30.verticalSpace,
-            TextButton(
-              onPressed: () => FirebaseAuth.instance.signOut(),
-              child: const Text('Sign Out'),
-            ),
-          ],
-        ),
       ),
     );
   }
