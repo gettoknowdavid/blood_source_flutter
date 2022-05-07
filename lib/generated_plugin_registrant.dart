4461ac2d7249bc1516dad14e28ae494bad1f9c7d
David Michael II <knowdavidmichael@gmail.com>
2022-05-07 03:43:54 +0100

    remove: sign_in_page and sign_up_page

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -9,6 +9,8 @@ import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_analytics_web/firebase_analytics_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
+import 'package:sign_in_with_apple_web/sign_in_with_apple_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -18,5 +20,7 @@ void registerPlugins(Registrar registrar) {
   FirebaseAnalyticsWeb.registerWith(registrar);
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
+  SignInWithApplePlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
