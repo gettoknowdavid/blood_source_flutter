8deb129b481a8391baed96dcc61838e36f910f9f
David Michael II <knowdavidmichael@gmail.com>
2022-05-07 03:42:45 +0100

    feat: create sign_in view

---
+++ lib/ui/views/sign_in/sign_in_view.dart
@@ -0,0 +1,116 @@
+import 'package:blood_source/common/app_colors.dart';
+import 'package:blood_source/common/header_painter.dart';
+import 'package:blood_source/shared/widgets/app_button.dart';
+import 'package:blood_source/shared/widgets/app_text_button.dart';
+import 'package:blood_source/shared/widgets/app_textfield.dart';
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import './sign_in_view_model.dart';
+
+class SignInView extends StatelessWidget {
+  const SignInView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<SignInViewModel>.reactive(
+      viewModelBuilder: () => SignInViewModel(),
+      onModelReady: (SignInViewModel model) async {
+        await model.init();
+      },
+      builder: (
+        BuildContext context,
+        SignInViewModel model,
+        Widget? child,
+      ) {
+        return Scaffold(
+          body: SafeArea(
+            child: Container(
+              color: AppColors.primary.withOpacity(0.03),
+              child: CustomPaint(
+                painter: HeaderPainter(),
+                child: Center(
+                  child: SingleChildScrollView(
+                    padding: EdgeInsets.symmetric(horizontal: 24.r),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          'Hello Again!',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 32.sp,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                          ),
+                        ),
+                        4.verticalSpace,
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 40.r),
+                          child: Text(
+                            "Welcome back, you've been missed!",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 18.sp,
+                              color: Colors.white70,
+                            ),
+                          ),
+                        ),
+                        40.verticalSpace,
+                        AppTextField(
+                          controller: model.emailController,
+                          keyboardType: TextInputType.emailAddress,
+                          hintText: 'Enter email',
+                        ),
+                        18.verticalSpace,
+                        AppTextField(
+                          controller: model.passwordController,
+                          hintText: 'Password',
+                          isPassword: true,
+                        ),
+                        AppTextButton(
+                          text: 'Password Recovery',
+                          onTap: () {},
+                          fontSize: 16.sp,
+                          color: Colors.black54,
+                          alignment: Alignment.centerRight,
+                        ),
+                        40.verticalSpace,
+                        AppButton(onTap: () => model.signIn(), text: 'Sign In'),
+                        30.verticalSpace,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Not a member?',
+                              style: TextStyle(
+                                fontSize: 17.sp,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black54,
+                              ),
+                            ),
+                            6.horizontalSpace,
+                            AppTextButton(
+                              text: 'Register now',
+                              onTap: () {},
+                              // onTap: () => Get.to(() => const SignUpPage()),
+                              fontSize: 17.sp,
+                              color: AppColors.swatch.shade500,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/ui/views/sign_in/sign_in_view_model.dart
@@ -0,0 +1,54 @@
+import 'package:blood_source/app/app.locator.dart';
+import 'package:blood_source/app/app.router.dart';
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_firebase_auth/stacked_firebase_auth.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class SignInViewModel extends StreamViewModel with ReactiveServiceMixin {
+  NavigationService? navigationService = locator<NavigationService>();
+  FirebaseAuthenticationService? authService =
+      locator<FirebaseAuthenticationService>();
+
+  final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
+  GlobalKey<FormState> get loginFormKey => _loginFormKey;
+
+  bool _isObscurePassword = false;
+  bool get isObscurePassword => _isObscurePassword;
+
+  Future<void> init() async {}
+
+  @override
+  Stream get stream => throw UnimplementedError();
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void initialise() {
+    notifyListeners();
+  }
+
+  void toggleObscurePassword() {
+    _isObscurePassword = !_isObscurePassword;
+    notifyListeners();
+  }
+
+  Future signIn() async {
+    await authService!.loginWithEmail(
+      email: emailController.text.trim(),
+      password: passwordController.text.trim(),
+    );
+
+    if (authService!.hasUser) {
+      navigationService!.navigateTo(Routes.homeView);
+    }
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+}
