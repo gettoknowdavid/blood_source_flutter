c2a8dcd1855aa8bd555cd680f9fcdf98b0d2189f
David Michael II <knowdavidmichael@gmail.com>
2022-05-07 21:31:57 +0100

    fix: sign out navigation

--- lib/ui/views/home/home_view_model.dart
+++ lib/ui/views/home/home_view_model.dart
@@ -1,8 +1,11 @@
 import 'package:blood_source/app/app.locator.dart';
+import 'package:blood_source/app/app.router.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_firebase_auth/stacked_firebase_auth.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 class HomeViewModel extends BaseViewModel {
+  NavigationService navService = locator<NavigationService>();
   FirebaseAuthenticationService authService =
       locator<FirebaseAuthenticationService>();
 
@@ -10,5 +13,11 @@ class HomeViewModel extends BaseViewModel {
 
   Future signOut() async {
     await authService.logout();
+
+    if (!authService.hasUser) {
+      navService.replaceWith(Routes.signInView);
+    }
+
+    notifyListeners();
   }
 }
