912862f75524cbf63ce5a5f91479a29a1033cf3e
David Michael II <knowdavidmichael@gmail.com>
2022-05-07 09:30:36 +0100

    fix: adjust header paint

--- lib/common/header_painter.dart
+++ lib/common/header_painter.dart
@@ -22,7 +22,7 @@ class HeaderPainter extends CustomPainter {
       ..strokeWidth = 8.0;
 
     canvas.drawCircle(
-        Offset(size.width * 0.26, size.height * 0.04), size.width * 0.7, paint);
+        Offset(size.width * 0.26, size.height * 0.02), size.width * 0.7, paint);
     // canvas.drawCircle(Offset(size.width * 0.98, size.height * -0.001),
     //     size.width * 0.45, paint2);
     canvas.drawCircle(Offset(size.width * 0.9, size.height * -0.01),
