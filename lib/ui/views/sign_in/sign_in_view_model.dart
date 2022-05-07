f84871bbd4367fb8de3b9e3c294dd1793baf6ed8
David Michael II <knowdavidmichael@gmail.com>
2022-05-07 21:22:06 +0100

    fix: email validation fix: signIn with authService

--- lib/ui/views/sign_in/sign_in_view_model.dart
+++ lib/ui/views/sign_in/sign_in_view_model.dart
@@ -6,6 +6,10 @@ import 'package:stacked_firebase_auth/stacked_firebase_auth.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class SignInViewModel extends BaseViewModel with ReactiveServiceMixin {
+  void initialise() {
+    notifyListeners();
+  }
+
   NavigationService navigationService = locator<NavigationService>();
   FirebaseAuthenticationService authService =
       locator<FirebaseAuthenticationService>();
@@ -18,30 +22,54 @@ class SignInViewModel extends BaseViewModel with ReactiveServiceMixin {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
+  String? signInError;
+
+  bool isFormValidated() {
+    if (emailController.text.isEmpty ||
+        passwordController.text.isEmpty ||
+        !RegExp(r'\S+@\S+\.\S+').hasMatch(emailController.text)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
   void goToSignUp() {
     navigationService.navigateTo(Routes.signUpView);
   }
 
-  emailValidator(text) {
-    if (text == null || text.isEmpty) {
-      return 'Email is required';
-    }
-    return null;
-  }
-
-  passwordValidator(text) {
-    if (text == null || text.isEmpty) {
-      return 'Password is required';
-    }
-    return null;
+  void onChanged(String? value) {
+    signInError = null;
+    notifyListeners();
   }
 
   Future signIn() async {
     if (_signInFormKey.currentState!.validate()) {
-      await authService.loginWithEmail(
+      FirebaseAuthenticationResult result = await authService.loginWithEmail(
         email: emailController.text.trim(),
         password: passwordController.text.trim(),
       );
+
+      if (result.user != null) {
+        navigationService.navigateTo(Routes.homeView);
+        notifyListeners();
+      }
+
+      if (result.hasError) {
+        switch (result.exceptionCode) {
+          case 'user-not-found':
+            signInError = 'No user found for this email.';
+            notifyListeners();
+            break;
+          case 'wrong-password':
+            signInError = 'Wrong email or password.';
+            notifyListeners();
+            break;
+          default:
+            '';
+        }
+        notifyListeners();
+      }
     }
 
     if (authService.hasUser) {
