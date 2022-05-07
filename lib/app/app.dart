c51e62e1c713aa252b0dbf145ff0ea00b890a3b3
David Michael II <knowdavidmichael@gmail.com>
2022-05-07 03:53:49 +0100

    feat: add new routes and dep

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,8 +2,12 @@ import 'package:blood_source/ui/views/home/home_view.dart';
 import 'package:blood_source/ui/views/home/home_view_model.dart';
 import 'package:blood_source/ui/views/sign_in/sign_in_view.dart';
 import 'package:blood_source/ui/views/sign_in/sign_in_view_model.dart';
+import 'package:blood_source/ui/views/sign_up/sign_up_view.dart';
+import 'package:blood_source/ui/views/sign_up/sign_up_view_model.dart';
 import 'package:blood_source/ui/views/splash/splash_view.dart';
 import 'package:blood_source/ui/views/splash/splash_view_model.dart';
+import 'package:blood_source/ui/views/verify_email/verify_email_view.dart';
+import 'package:blood_source/ui/views/verify_email/verify_email_view_model.dart';
 import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_firebase_auth/stacked_firebase_auth.dart';
 import 'package:stacked_services/stacked_services.dart';
@@ -12,14 +16,18 @@ import 'package:stacked_services/stacked_services.dart';
   routes: [
     MaterialRoute(page: SplashView, initial: true),
     MaterialRoute(page: SignInView),
+    MaterialRoute(page: SignUpView),
     MaterialRoute(page: HomeView),
+    MaterialRoute(page: VerifyEmailView),
   ],
   dependencies: [
     LazySingleton(classType: NavigationService),
     LazySingleton(classType: FirebaseAuthenticationService),
     LazySingleton(classType: SplashViewModel),
     LazySingleton(classType: SignInViewModel),
+    LazySingleton(classType: SignUpViewModel),
     LazySingleton(classType: HomeViewModel),
+    LazySingleton(classType: VerifyEmailViewModel),
   ],
 )
 class App {}
