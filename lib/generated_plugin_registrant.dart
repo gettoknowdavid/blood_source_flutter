d3dff961e3abda587ca6b467b5a61d124e503182
David Michael II <knowdavidmichael@gmail.com>
2022-05-08 01:10:00 +0100

    package: add open_mail_app and url_launcher

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -11,6 +11,7 @@ import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:sign_in_with_apple_web/sign_in_with_apple_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -22,5 +23,6 @@ void registerPlugins(Registrar registrar) {
   FirebaseCoreWeb.registerWith(registrar);
   GoogleSignInPlugin.registerWith(registrar);
   SignInWithApplePlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
