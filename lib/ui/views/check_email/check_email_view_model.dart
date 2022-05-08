01da84f35c4bbca6966eb09eda37fa5d350f86b6
David Michael II <knowdavidmichael@gmail.com>
2022-05-08 01:28:57 +0100

    feat: add mail_app_service to check_email view

--- lib/ui/views/check_email/check_email_view_model.dart
+++ lib/ui/views/check_email/check_email_view_model.dart
@@ -1,7 +1,15 @@
+import 'package:blood_source/app/app.locator.dart';
+import 'package:blood_source/services/mail_app_service.dart';
 import 'package:stacked/stacked.dart';
 
 class CheckEmailViewModel extends BaseViewModel {
+  MailAppService mailService = locator<MailAppService>();
+
   Future<void> init() async {}
+
+  Future openMailApp() async {
+    await mailService.openMailApp();
+  }
 
   void initialise() {
     notifyListeners();
