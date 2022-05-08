69faf62217825e2e00ac44aa7b4983058b2f2bdd
David Michael II <knowdavidmichael@gmail.com>
2022-05-08 02:36:40 +0100

    feat: custom dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:blood_source/app/app.locator.dart';
 import 'package:blood_source/app/app.router.dart';
 import 'package:blood_source/common/app_themes.dart';
+import 'package:blood_source/ui/shared/setup_dialog_ui.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -11,6 +12,7 @@ void main() async {
   await Firebase.initializeApp();
   await ScreenUtil.ensureScreenSize();
   setupLocator();
+  setupDialogUi();
 
   runApp(const MyApp());
 }
