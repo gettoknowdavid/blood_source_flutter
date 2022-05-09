fd736ba3086dc9d6ffe5f50d304eb214002c343a
David Michael II <knowdavidmichael@gmail.com>
2022-05-09 12:45:56 +0100

    feat: create password rules widget

---
+++ lib/ui/shared/widgets/sign_up/password_rules_widget.dart
@@ -0,0 +1,45 @@
+import 'package:blood_source/common/app_colors.dart';
+import 'package:blood_source/ui/views/sign_up/sign_up_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class PasswordRulesWidget extends ViewModelWidget<SignUpViewModel> {
+  const PasswordRulesWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, SignUpViewModel viewModel) {
+    return Visibility(
+      visible: viewModel.rulesVisibility,
+      child: Column(
+        children: [
+          ...viewModel.rules.map((e) {
+            final Color ruleColor = viewModel.passwordController.text.isEmpty
+                ? Theme.of(context).primaryColorDark.withOpacity(0.3)
+                : !e['rule'](
+                        viewModel.passwordController.text,
+                        viewModel.nameController.text,
+                        viewModel.emailController.text)
+                    ? AppColors.swatch.shade900
+                    : Colors.green;
+
+            return Row(
+              children: [
+                Icon(
+                  Icons.check_circle,
+                  size: 14.sp,
+                  color: ruleColor,
+                ),
+                10.verticalSpace,
+                Text(
+                  e["name"].toString(),
+                  style: TextStyle(fontSize: 14.sp, color: ruleColor),
+                )
+              ],
+            );
+          }).toList(),
+        ],
+      ),
+    );
+  }
+}
