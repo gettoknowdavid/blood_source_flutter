313587562cad303de80c83b9e63d8a0f10f25eb4
David Michael II <knowdavidmichael@gmail.com>
2022-05-09 13:58:44 +0100

    fix: verify layout

--- lib/ui/views/verify_email/verify_email_view.dart
+++ lib/ui/views/verify_email/verify_email_view.dart
@@ -1,7 +1,8 @@
 import 'package:blood_source/common/app_colors.dart';
-import 'package:blood_source/common/header_painter.dart';
 import 'package:blood_source/common/image_resources.dart';
+import 'package:blood_source/ui/layouts/auth_layout.dart';
 import 'package:blood_source/ui/shared/widgets/app_text_button.dart';
+import 'package:blood_source/ui/shared/widgets/auth_layout_header.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -19,68 +20,39 @@ class VerifyEmailView extends StatelessWidget {
         await model.init();
       },
       builder: (context, model, Widget? child) {
-        return Scaffold(
-          body: SafeArea(
-            child: Container(
-              color: AppColors.primary.withOpacity(0.03),
-              child: CustomPaint(
-                painter: HeaderPainter(),
-                child: Center(
-                  child: SingleChildScrollView(
-                    padding: EdgeInsets.symmetric(horizontal: 24.r),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text(
-                          'Email Verification',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 32.sp,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.white,
-                          ),
-                        ),
-                        4.verticalSpace,
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 40.r),
-                          child: Text(
-                            "A verification email has been sent to your email, please verify your account.",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 18.sp,
-                              color: Colors.white70,
-                            ),
-                          ),
-                        ),
-                        40.verticalSpace,
-                        Image.asset(ImageResources.verifyEmail),
-                        30.verticalSpace,
-                        Text(
-                          model.isVerified ? 'Verified' : 'Not Verified',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 19.sp,
-                            fontWeight: FontWeight.w600,
-                            fontStyle: FontStyle.italic,
-                            color: model.isVerified
-                                ? Colors.green
-                                : Colors.black54,
-                          ),
-                        ),
-                        6.verticalSpace,
-                        AppTextButton(
-                          onTap: () {},
-                          text: 'Resend Email',
-                          fontSize: 19.sp,
-                          color: AppColors.swatch.shade900,
-                          fontWeight: FontWeight.w600,
-                        )
-                      ],
-                    ),
+        return AuthLayout(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const AuthLayoutHeader(
+                  title: 'Email Verification',
+                  subtitle:
+                      "A verification email has been sent to your email, please verify your account.",
+                ),
+                20.verticalSpace,
+                Image.asset(ImageResources.verifyEmail),
+                30.verticalSpace,
+                Text(
+                  model.isVerified ? 'Verified' : 'Not Verified',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w600,
+                    fontStyle: FontStyle.italic,
+                    color: model.isVerified ? Colors.green : Colors.black54,
                   ),
                 ),
-              ),
+                6.verticalSpace,
+                AppTextButton(
+                  onTap: () {},
+                  text: 'Resend Email',
+                  fontSize: 14.sp,
+                  color: AppColors.swatch.shade900,
+                  fontWeight: FontWeight.w600,
+                )
+              ],
             ),
           ),
         );
