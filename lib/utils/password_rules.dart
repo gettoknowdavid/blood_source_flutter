96415c4cfb1849ef84ff926f5e042cb70cccad4d
David Michael II <knowdavidmichael@gmail.com>
2022-05-10 22:39:43 +0100

    fix: passwordrules

--- lib/utils/password_rules.dart
+++ lib/utils/password_rules.dart
@@ -1,19 +1,19 @@
 List<Map> passwordRules = [
   {
-    'name': 'Must not contain your name or email',
+    'name': 'Not your name or email',
     'rule': (String password, String name, String email) {
       return !(password.toLowerCase().contains(name.toLowerCase()) ||
           password.toLowerCase().contains(email.toLowerCase()));
     }
   },
   {
-    'name': 'Must have 8 characters & 1 uppercase letter',
+    'name': '8 characters & 1 uppercase letter',
     'rule': (String password, String name, String email) {
       return RegExp("^(?=.*?[A-Z]).{8,}\$").hasMatch(password);
     }
   },
   {
-    'name': 'Must contain a special character & a number',
+    'name': 'A special character & a number',
     'rule': (String password, String name, String email) {
       return !RegExp("^[a-zA-Z0-9 ]*\$").hasMatch(password);
     }
