74bb38f9067524a155a9360e1236a7babe3bacb6
David Michael II <knowdavidmichael@gmail.com>
2022-05-10 22:55:32 +0100

    feat: add StreamViewModel to splash view model

--- lib/ui/views/splash/splash_view_model.dart
+++ lib/ui/views/splash/splash_view_model.dart
@@ -1,5 +1,16 @@
+import 'package:blood_source/app/app.locator.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_firebase_auth/stacked_firebase_auth.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 class SplashViewModel extends BaseViewModel {
   Future<void> init() async {}
+
+  NavigationService navService = locator<NavigationService>();
+  FirebaseAuthenticationService authService =
+      locator<FirebaseAuthenticationService>();
+
+  bool isVerified() {
+    return authService.currentUser!.emailVerified;
+  }
 }
