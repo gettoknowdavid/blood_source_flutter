5302afa375ae21e0eaef0380bcf36ac08506d7c6
David Michael II <knowdavidmichael@gmail.com>
2022-05-11 13:51:43 +0100

    fix: auth verification stream

--- lib/ui/views/splash/splash_view_model.dart
+++ lib/ui/views/splash/splash_view_model.dart
@@ -1,9 +1,10 @@
 import 'package:blood_source/app/app.locator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_firebase_auth/stacked_firebase_auth.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-class SplashViewModel extends BaseViewModel {
+class SplashViewModel extends StreamViewModel<User?> {
   Future<void> init() async {}
 
   NavigationService navService = locator<NavigationService>();
@@ -13,4 +14,7 @@ class SplashViewModel extends BaseViewModel {
   bool isVerified() {
     return authService.currentUser!.emailVerified;
   }
+
+  @override
+  Stream<User?> get stream => authService.authStateChanges;
 }
