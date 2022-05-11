36d256541a391b7beb2473e67bc370737f00bbcb
David Michael II <knowdavidmichael@gmail.com>
2022-05-11 20:39:03 +0100

    feat: add form key to donor form

--- lib/ui/views/donor_form/donor_form_view_model.dart
+++ lib/ui/views/donor_form/donor_form_view_model.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
 class DonorFormViewModel extends BaseViewModel {
   Future<void> init() async {}
+
+  final GlobalKey<FormState> _donorFormKey = GlobalKey<FormState>();
+  GlobalKey<FormState> get donorFormKey => _donorFormKey;
 }
