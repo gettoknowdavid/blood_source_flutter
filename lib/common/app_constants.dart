2bce6426c3a3e7baf54bd164e60f4c2da56c3e54
David Michael II <knowdavidmichael@gmail.com>
2022-05-13 21:29:17 +0100

    feat: create app constants

---
+++ lib/common/app_constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  static const unqualifiedMessage =
+      'Sorry, you are currently not eligible to be a blood donor. You will be registered as a recipient.';
+  static const qualifiedMessage =
+      'Congratulations, you are eligible to be a donor.';
+}
