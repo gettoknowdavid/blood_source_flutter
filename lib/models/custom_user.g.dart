94f0c00d553438fffaa7207e9d34f17607491027
David Michael II <knowdavidmichael@gmail.com>
2022-05-13 21:31:11 +0100

    feat: generate json serializable for custom_user

---
+++ lib/models/custom_user.g.dart
@@ -0,0 +1,34 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'custom_user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CustomUser _$CustomUserFromJson(Map<String, dynamic> json) => CustomUser(
+      age: json['age'] as int,
+      weight: json['weight'] as int,
+      bloodGroup: json['bloodGroup'] as String,
+      diseases:
+          (json['diseases'] as List<dynamic>).map((e) => e as String).toList(),
+      piercingOrTattoo: json['piercingOrTattoo'] as bool,
+      pregnantOrBreastFeeding: json['pregnantOrBreastFeeding'] as bool,
+      userType: $enumDecode(_$UserTypeEnumMap, json['userType']),
+    );
+
+Map<String, dynamic> _$CustomUserToJson(CustomUser instance) =>
+    <String, dynamic>{
+      'age': instance.age,
+      'weight': instance.weight,
+      'bloodGroup': instance.bloodGroup,
+      'diseases': instance.diseases,
+      'piercingOrTattoo': instance.piercingOrTattoo,
+      'pregnantOrBreastFeeding': instance.pregnantOrBreastFeeding,
+      'userType': _$UserTypeEnumMap[instance.userType],
+    };
+
+const _$UserTypeEnumMap = {
+  UserType.donor: 'donor',
+  UserType.recipient: 'recipient',
+};
