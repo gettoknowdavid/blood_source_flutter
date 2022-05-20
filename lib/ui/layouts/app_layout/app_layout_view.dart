72a9c2e94854bf53f3407e826be086ae5eac0fd7
David Michael II <knowdavidmichael@gmail.com>
2022-05-20 21:42:13 +0100

    feat: create app_layout

---
+++ lib/ui/layouts/app_layout/app_layout_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+import './app_layout_view_model.dart';
+
+class AppLayoutView extends StatelessWidget {
+  const AppLayoutView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<AppLayoutViewModel>.reactive(
+      viewModelBuilder: () => AppLayoutViewModel(),
+      onModelReady: (model) async => await model.init(),
+      builder: (context, model, Widget? child) {
+        return Scaffold(
+          body: Center(
+            child: Text(
+              'AppLayoutView',
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
