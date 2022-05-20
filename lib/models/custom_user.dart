5fdf3a2901b76ebd155a6635de0b06834e8a835f
David Michael II <knowdavidmichael@gmail.com>
2022-05-20 17:39:01 +0100

    build: add null safety to custom_user model

--- lib/models/custom_user.dart
+++ lib/models/custom_user.dart
@@ -7,42 +7,42 @@ part 'custom_user.g.dart';
 @JsonSerializable(explicitToJson: true)
 class CustomUser {
   CustomUser({
-    required this.age,
-    required this.weight,
-    required this.bloodGroup,
-    required this.diseases,
-    required this.piercingOrTattoo,
-    required this.pregnantOrBreastFeeding,
+    this.age,
+    this.weight,
+    this.bloodGroup,
+    this.diseases,
+    this.piercingOrTattoo,
+    this.pregnantOrBreastFeeding,
     required this.userType,
-    required this.isDonorEligible,
-    required this.isDonorFormComplete,
+    this.isDonorEligible,
+    this.isDonorFormComplete = false,
   });
 
   /// The age of the donor which should not be
   /// below 18 or above 60.
   /// For recipients, any age is allowed.
-  final int age;
+  final int? age;
 
   /// The weight of the donor which must be
   /// greater than 45kg
-  final int weight;
+  final int? weight;
 
   /// The [BloodGroup] of the donor
-  final String bloodGroup;
+  final String? bloodGroup;
 
   /// A [List<Disease>] which the user may or may
   /// not have had
-  final List<String> diseases;
+  final List<String>? diseases;
 
   /// Returns a bool of either true or
   /// false if the user has had any piercings
   /// or tattoos in the last 6 months
-  final bool piercingOrTattoo;
+  final bool? piercingOrTattoo;
 
   /// Returns a bool of either true or
   /// false if the user is pregnant and/or breast
   /// feeding
-  final bool pregnantOrBreastFeeding;
+  final bool? pregnantOrBreastFeeding;
 
   /// The [UserType] of the application user
   final UserType userType;
@@ -50,7 +50,7 @@ class CustomUser {
   /// Returns true if the user has met the donor
   /// requirements.
   /// Will return false otherwise.
-  final bool isDonorEligible;
+  final bool? isDonorEligible;
 
   /// Returns true if the user has filled the donor
   /// requirements form.
@@ -64,17 +64,18 @@ class CustomUser {
   CustomUser.fromFirestore(
     DocumentSnapshot<Map<String, dynamic>> snapshot,
     SnapshotOptions? options,
-  )   : age = snapshot.data()?["age"],
-        weight = snapshot.data()?["weight"],
-        bloodGroup = snapshot.data()?["bloodGroup"],
-        diseases = (snapshot.data()?["diseases"] as List<dynamic>)
-            .map((e) => e as String)
+  )   : age = snapshot.data()?["age"] as int?,
+        weight = snapshot.data()?["weight"] as int?,
+        bloodGroup = snapshot.data()?["bloodGroup"] as String?,
+        diseases = (snapshot.data()?["diseases"] as List<dynamic>?)
+            ?.map((e) => e as String)
             .toList(),
-        piercingOrTattoo = snapshot.data()?["piercingOrTattoo"],
-        pregnantOrBreastFeeding = snapshot.data()?["pregnantOrBreastFeeding"],
-        userType = snapshot.data()?["userType"],
-        isDonorEligible = snapshot.data()?["isDonorEligible"],
-        isDonorFormComplete = snapshot.data()?["isDonorFormComplete"];
+        piercingOrTattoo = snapshot.data()?["piercingOrTattoo"] as bool?,
+        pregnantOrBreastFeeding =
+            snapshot.data()?["pregnantOrBreastFeeding"] as bool?,
+        userType = $enumDecode(_$UserTypeEnum, snapshot.data()?['userType']),
+        isDonorEligible = snapshot.data()?["isDonorEligible"] as bool?,
+        isDonorFormComplete = snapshot.data()?["isDonorFormComplete"] as bool;
 
   Map<String, dynamic> toFirestore() {
     return {
@@ -85,10 +86,15 @@ class CustomUser {
       if (piercingOrTattoo != null) "piercingOrTattoo": piercingOrTattoo,
       if (pregnantOrBreastFeeding != null)
         "pregnantOrBreastFeeding": pregnantOrBreastFeeding,
-      if (userType != null) "isDonorEligible": userType,
+      if (userType != null) "userType": _$UserTypeEnum[userType],
       if (isDonorEligible != null) "isDonorEligible": isDonorEligible,
       if (isDonorFormComplete != null)
         "isDonorFormComplete": isDonorFormComplete,
     };
   }
 }
+
+const _$UserTypeEnum = {
+  UserType.donor: 'donor',
+  UserType.recipient: 'recipient',
+};
