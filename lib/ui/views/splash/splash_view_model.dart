23f52f2ce13cdbd8198adff744e32df8e0380501
David Michael II <knowdavidmichael@gmail.com>
2022-05-20 19:32:18 +0100

    feat: update navigation after splash

--- lib/ui/views/splash/splash_view_model.dart
+++ lib/ui/views/splash/splash_view_model.dart
@@ -1,11 +1,17 @@
+import 'dart:async';
+
 import 'package:blood_source/app/app.locator.dart';
+import 'package:blood_source/app/app.router.dart';
+import 'package:blood_source/models/custom_user.dart';
+import 'package:blood_source/models/user-type.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_firebase_auth/stacked_firebase_auth.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class SplashViewModel extends StreamViewModel<User?> {
-  Future<void> init() async {}
+  Timer? timer;
 
   NavigationService navService = locator<NavigationService>();
   FirebaseAuthenticationService authService =
@@ -17,4 +23,59 @@ class SplashViewModel extends StreamViewModel<User?> {
 
   @override
   Stream<User?> get stream => authService.authStateChanges;
+
+  Future checkVerification() async {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) async {
+      if (user == null) {
+        timer?.cancel();
+        navService.clearStackAndShow(Routes.signInView);
+      }
+
+      if (user != null) {
+        timer?.cancel();
+        await FirebaseAuth.instance.currentUser!.reload();
+
+        if (!isVerified()) {
+          navService.clearStackAndShow(Routes.verifyEmailView);
+        }
+
+        if (isVerified()) {
+          final user = FirebaseAuth.instance.currentUser!.uid;
+          final _ref = FirebaseFirestore.instance
+              .collection('users')
+              .doc(user)
+              .withConverter<CustomUser>(
+                  fromFirestore: CustomUser.fromFirestore,
+                  toFirestore: (_cs, _) => _cs.toFirestore());
+
+          final _docSnap = await _ref.get();
+          final _cUser = _docSnap.data();
+
+          if (_cUser!.userType == UserType.donor &&
+              !_cUser.isDonorFormComplete) {
+            navService.clearStackAndShow(Routes.donorFormView);
+            notifyListeners();
+          }
+
+          if (_cUser.userType == UserType.donor && _cUser.isDonorFormComplete) {
+            navService.clearStackAndShow(Routes.homeView);
+            notifyListeners();
+          }
+
+          if (_cUser.userType == UserType.recipient) {
+            navService.clearStackAndShow(Routes.homeView);
+            notifyListeners();
+          }
+
+          notifyListeners();
+        }
+      }
+    });
+  }
+
+  Future<void> init() async {
+    timer = Timer.periodic(const Duration(seconds: 5), (_) async {
+      await checkVerification();
+    });
+  }
 }
