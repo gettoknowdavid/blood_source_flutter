e571658e142b2c409f60793595a837c6e088ee47
David Michael II <knowdavidmichael@gmail.com>
2022-05-23 11:19:00 +0100

    refactor: dashboard_button_item

--- lib/ui/views/dashboard/dashboard_view.dart
+++ lib/ui/views/dashboard/dashboard_view.dart
@@ -1,5 +1,5 @@
 import 'package:blood_source/common/app_colors.dart';
-import 'package:blood_source/models/dashboard_item.dart';
+import 'package:blood_source/models/dashboard_button_model.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -60,54 +60,7 @@ class DashboardView extends StatelessWidget {
                   ),
                   itemCount: buttonList.length,
                   itemBuilder: (BuildContext context, i) {
-                    return Container(
-                      alignment: Alignment.center,
-                      height: 0.5 * 1.sh,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(16.r),
-                        boxShadow: [
-                          BoxShadow(
-                            blurRadius: 10,
-                            spreadRadius: 10,
-                            color: Colors.black.withOpacity(0.01),
-                          )
-                        ],
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            alignment: Alignment.center,
-                            padding: EdgeInsets.all(24.r),
-                            child: buttonList[i].icon,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: buttonList[i]
-                                  .backgroundColor
-                                  .withOpacity(0.2),
-                            ),
-                          ),
-                          12.verticalSpace,
-                          Text(
-                            buttonList[i].title,
-                            style: TextStyle(fontSize: 16.sp),
-                          ),
-                          15.verticalSpace,
-                          Container(
-                            padding: EdgeInsets.symmetric(horizontal: 12.r),
-                            decoration: BoxDecoration(
-                              color: Colors.blue[300],
-                              borderRadius: BorderRadius.circular(16.r),
-                            ),
-                            child: Text(
-                              buttonList[i].subtitle,
-                              style: const TextStyle(color: Colors.white),
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
+                    return DashboardButtonItem(model: buttonList[i]);
                   },
                 ),
               ),
@@ -115,6 +68,62 @@ class DashboardView extends StatelessWidget {
           ),
         );
       },
+    );
+  }
+}
+
+class DashboardButtonItem extends StatelessWidget {
+  const DashboardButtonItem({Key? key, required this.model}) : super(key: key);
+
+  final DashboardButtonModel model;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      height: 0.5 * 1.sh,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16.r),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 10,
+            spreadRadius: 10,
+            color: Colors.black.withOpacity(0.01),
+          )
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(24.r),
+            child: model.icon,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: model.backgroundColor.withOpacity(0.2),
+            ),
+          ),
+          12.verticalSpace,
+          Text(
+            model.title,
+            style: TextStyle(fontSize: 16.sp),
+          ),
+          15.verticalSpace,
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 12.r),
+            decoration: BoxDecoration(
+              color: Colors.blue[300],
+              borderRadius: BorderRadius.circular(16.r),
+            ),
+            child: Text(
+              model.subtitle,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
