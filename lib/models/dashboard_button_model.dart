ae5085a10f3ef0e43f2e838dee97c9d8486849db
David Michael II <knowdavidmichael@gmail.com>
2022-05-23 11:26:13 +0100

    feat: add route to dashboard_button_model

--- lib/models/dashboard_button_model.dart
+++ lib/models/dashboard_button_model.dart
@@ -1,3 +1,4 @@
+import 'package:blood_source/app/app.router.dart';
 import 'package:blood_source/common/app_colors.dart';
 import 'package:flutter/material.dart';
 
@@ -6,9 +7,15 @@ class DashboardButtonModel {
   final String subtitle;
   final Widget icon;
   final Color backgroundColor;
+  final String route;
 
   DashboardButtonModel(
-      this.title, this.subtitle, this.icon, this.backgroundColor);
+    this.title,
+    this.subtitle,
+    this.icon,
+    this.backgroundColor,
+    this.route,
+  );
 }
 
 final List<DashboardButtonModel> buttonList = <DashboardButtonModel>[
@@ -17,6 +24,7 @@ final List<DashboardButtonModel> buttonList = <DashboardButtonModel>[
     '22k',
     const Icon(Icons.search, color: AppColors.primary),
     const Color(0xFFFA6393),
+    Routes.donateView,
   ),
   DashboardButtonModel(
     'Request Blood',
@@ -26,6 +34,7 @@ final List<DashboardButtonModel> buttonList = <DashboardButtonModel>[
       color: AppColors.primary,
     ),
     const Color(0xFFFA6393),
+    Routes.donateView,
   ),
   DashboardButtonModel(
     'Blood Bank',
@@ -35,6 +44,7 @@ final List<DashboardButtonModel> buttonList = <DashboardButtonModel>[
       color: Color(0xFF00CC99),
     ),
     const Color(0xFF00CC99),
+    Routes.donateView,
   ),
   DashboardButtonModel(
     'Others',
@@ -44,5 +54,6 @@ final List<DashboardButtonModel> buttonList = <DashboardButtonModel>[
       color: Color(0xFF999999),
     ),
     const Color(0xFF999999),
+    Routes.donateView,
   ),
 ];
