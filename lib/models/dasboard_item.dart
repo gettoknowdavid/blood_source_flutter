a57fc897750935d4b77fa2e90c0303dbeb6a656c
David Michael II <knowdavidmichael@gmail.com>
2022-05-23 11:09:33 +0100

    feat: add button list

--- lib/models/dasboard_item.dart
+++ lib/models/dasboard_item.dart
@@ -1,3 +1,4 @@
+import 'package:blood_source/common/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class DashboardItem {
@@ -8,3 +9,39 @@ class DashboardItem {
 
   DashboardItem(this.title, this.subtitle, this.icon, this.backgroundColor);
 }
+
+final List<DashboardItem> buttonList = <DashboardItem>[
+  DashboardItem(
+    'Find a Donor',
+    '22k',
+    const Icon(Icons.search, color: AppColors.primary),
+    const Color(0xFFFA6393),
+  ),
+  DashboardItem(
+    'Request Blood',
+    '32k',
+    const ImageIcon(
+      AssetImage('assets/images/bell.png'),
+      color: AppColors.primary,
+    ),
+    const Color(0xFFFA6393),
+  ),
+  DashboardItem(
+    'Blood Bank',
+    'Map',
+    const ImageIcon(
+      AssetImage('assets/images/blood.png'),
+      color: Color(0xFF00CC99),
+    ),
+    const Color(0xFF00CC99),
+  ),
+  DashboardItem(
+    'Others',
+    'More',
+    const ImageIcon(
+      AssetImage('assets/images/settings.png'),
+      color: Color(0xFF999999),
+    ),
+    const Color(0xFF999999),
+  ),
+];
