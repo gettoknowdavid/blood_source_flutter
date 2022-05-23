528b1cb9a864e4c49622cbd9c2d8d763792441ef
David Michael II <knowdavidmichael@gmail.com>
2022-05-23 10:00:15 +0100

    feat: add app_icons

---
+++ lib/common/app_icons.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppIcons {
+  static ImageIcon bell = ImageIcon(
+    const AssetImage('assets/images/bell.png'),
+    size: 22.sp,
+  );
+  static ImageIcon blood = ImageIcon(
+    const AssetImage('assets/images/blood.png'),
+    size: 22.sp,
+    color: Colors.white,
+  );
+  static ImageIcon home = ImageIcon(
+    const AssetImage('assets/images/home.png'),
+    size: 22.sp,
+  );
+  static ImageIcon profile = ImageIcon(
+    const AssetImage('assets/images/profile.png'),
+    size: 22.sp,
+  );
+  static ImageIcon settings = ImageIcon(
+    const AssetImage('assets/images/settings.png'),
+    size: 22.sp,
+  );
+}
