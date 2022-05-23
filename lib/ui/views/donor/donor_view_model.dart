10f2cbb6fd6c6e3b3743a9e1864d410097cb1122
David Michael II <knowdavidmichael@gmail.com>
2022-05-23 21:43:23 +0100

    feat: get donors from cloudstore

--- lib/ui/views/donor/donor_view_model.dart
+++ lib/ui/views/donor/donor_view_model.dart
@@ -1,5 +1,39 @@
+import 'package:blood_source/models/blood_source_user.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:stacked/stacked.dart';
 
-class DonorViewModel extends BaseViewModel {
-  Future<void> init() async {}
+class DonorViewModel extends BaseViewModel with ReactiveServiceMixin {
+  DonorViewModel() {
+    listenToReactiveValues([_donors, _donorCount]);
+  }
+
+  final ReactiveValue<List<BloodSourceUser>> _donors =
+      ReactiveValue<List<BloodSourceUser>>([]);
+  List<BloodSourceUser> get donors => _donors.value;
+  final ReactiveValue<int> _donorCount = ReactiveValue<int>(0);
+  int get donorCount => _donorCount.value;
+
+  Future<void> init() async {
+    getDonors();
+    getDonorCountString();
+  }
+
+  String getDonorCountString() {
+    if (_donorCount.value > 1) {
+      return '${_donorCount.value} donor are available';
+    } else {
+      return '${_donorCount.value} donors are available';
+    }
+  }
+
+  List<BloodSourceUser> getDonors() {
+    FirebaseFirestore.instance.collection("donors").snapshots().listen((event) {
+      for (var doc in event.docs) {
+        _donors.value.add(BloodSourceUser.fromFirestore(doc, null));
+      }
+      _donorCount.value = _donors.value.length;
+    });
+
+    return _donors.value;
+  }
 }
