af46e1181d4e779e466a6e8ccf7ae0ca37d78533
David Michael II <knowdavidmichael@gmail.com>
2022-05-23 11:08:35 +0100

    feat: add dashboard_item

---
+++ lib/models/dasboard_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DashboardItem {
+  final String title;
+  final String subtitle;
+  final Widget icon;
+  final Color backgroundColor;
+
+  DashboardItem(this.title, this.subtitle, this.icon, this.backgroundColor);
+}
