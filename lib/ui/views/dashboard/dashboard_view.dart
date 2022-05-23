ed234b63e501aca1e9fa54acabf8db8a50b3dad2
David Michael II <knowdavidmichael@gmail.com>
2022-05-23 11:21:25 +0100

    feat: create dashboard_button_item

--- lib/ui/views/dashboard/dashboard_view.dart
+++ lib/ui/views/dashboard/dashboard_view.dart
@@ -1,5 +1,6 @@
 import 'package:blood_source/common/app_colors.dart';
 import 'package:blood_source/models/dashboard_button_model.dart';
+import 'package:blood_source/ui/shared/widgets/dahsboard_view/dashboard_button_item.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -68,62 +69,6 @@ class DashboardView extends StatelessWidget {
           ),
         );
       },
-    );
-  }
-}
-
-class DashboardButtonItem extends StatelessWidget {
-  const DashboardButtonItem({Key? key, required this.model}) : super(key: key);
-
-  final DashboardButtonModel model;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      height: 0.5 * 1.sh,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16.r),
-        boxShadow: [
-          BoxShadow(
-            blurRadius: 10,
-            spreadRadius: 10,
-            color: Colors.black.withOpacity(0.01),
-          )
-        ],
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.all(24.r),
-            child: model.icon,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: model.backgroundColor.withOpacity(0.2),
-            ),
-          ),
-          12.verticalSpace,
-          Text(
-            model.title,
-            style: TextStyle(fontSize: 16.sp),
-          ),
-          15.verticalSpace,
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 12.r),
-            decoration: BoxDecoration(
-              color: Colors.blue[300],
-              borderRadius: BorderRadius.circular(16.r),
-            ),
-            child: Text(
-              model.subtitle,
-              style: const TextStyle(color: Colors.white),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
