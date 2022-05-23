b77e4798c05ac04643d0b6b4185d785d4b5c6e28
David Michael II <knowdavidmichael@gmail.com>
2022-05-23 15:38:04 +0100

    refactor: donor_list_item

--- lib/ui/views/donor/donor_view.dart
+++ lib/ui/views/donor/donor_view.dart
@@ -1,6 +1,7 @@
 import 'package:blood_source/common/app_colors.dart';
 import 'package:blood_source/common/app_icons.dart';
 import 'package:blood_source/ui/shared/widgets/app_back_button.dart';
+import 'package:blood_source/ui/shared/widgets/donor/donor_list_item.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -111,89 +112,6 @@ class DonorView extends StatelessWidget {
           ),
         );
       },
-    );
-  }
-}
-
-class DonorListItem extends StatelessWidget {
-  const DonorListItem({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 80.h,
-      width: 1.sw,
-      padding: EdgeInsets.symmetric(
-        vertical: 10.r,
-        horizontal: 16.r,
-      ),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12.r),
-        boxShadow: [
-          BoxShadow(
-            blurRadius: 18,
-            spreadRadius: 0,
-            color: Colors.black12,
-            offset: Offset(0, 10.h),
-          )
-        ],
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            radius: 25.w,
-            backgroundColor: AppColors.swatch.shade400,
-          ),
-          10.horizontalSpace,
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Jaber Khalaf',
-                style: TextStyle(fontSize: 17.sp),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Icon(
-                    Icons.location_pin,
-                    color: AppColors.secondary,
-                    size: 18.sp,
-                  ),
-                  Text(
-                    'Tulkarm',
-                    style: TextStyle(
-                      fontSize: 12.sp,
-                      color: Colors.black87,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          const Spacer(),
-          Container(
-            height: 46.h,
-            width: 46.h,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.r),
-              color: const Color(0xFF826DEE),
-            ),
-            child: Text(
-              '5km',
-              style: TextStyle(
-                fontSize: 12.sp,
-                fontWeight: FontWeight.w600,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
