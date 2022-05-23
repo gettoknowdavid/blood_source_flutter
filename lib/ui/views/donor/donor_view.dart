f4435c6d6fe860ab55b84248b78ab40b3b1f8ecc
David Michael II <knowdavidmichael@gmail.com>
2022-05-23 15:35:49 +0100

    refactor: donor list item

--- lib/ui/views/donor/donor_view.dart
+++ lib/ui/views/donor/donor_view.dart
@@ -103,81 +103,7 @@ class DonorView extends StatelessWidget {
                   shrinkWrap: true,
                   padding: EdgeInsets.symmetric(horizontal: 26.r),
                   children: [
-                    Container(
-                      height: 80.h,
-                      width: 1.sw,
-                      padding: EdgeInsets.symmetric(
-                        vertical: 10.r,
-                        horizontal: 16.r,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(12.r),
-                        boxShadow: [
-                          BoxShadow(
-                            blurRadius: 18,
-                            spreadRadius: 0,
-                            color: Colors.black12,
-                            offset: Offset(0, 10.h),
-                          )
-                        ],
-                      ),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          CircleAvatar(
-                            radius: 25.w,
-                            backgroundColor: AppColors.swatch.shade400,
-                          ),
-                          10.horizontalSpace,
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Jaber Khalaf',
-                                style: TextStyle(fontSize: 17.sp),
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Icon(
-                                    Icons.location_pin,
-                                    color: AppColors.secondary,
-                                    size: 18.sp,
-                                  ),
-                                  Text(
-                                    'Tulkarm',
-                                    style: TextStyle(
-                                      fontSize: 12.sp,
-                                      color: Colors.black87,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                          const Spacer(),
-                          Container(
-                            height: 46.h,
-                            width: 46.h,
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10.r),
-                              color: const Color(0xFF826DEE),
-                            ),
-                            child: Text(
-                              '5km',
-                              style: TextStyle(
-                                fontSize: 12.sp,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                    DonorListItem(),
                   ],
                 ),
               ],
@@ -185,6 +111,89 @@ class DonorView extends StatelessWidget {
           ),
         );
       },
+    );
+  }
+}
+
+class DonorListItem extends StatelessWidget {
+  const DonorListItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80.h,
+      width: 1.sw,
+      padding: EdgeInsets.symmetric(
+        vertical: 10.r,
+        horizontal: 16.r,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12.r),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 18,
+            spreadRadius: 0,
+            color: Colors.black12,
+            offset: Offset(0, 10.h),
+          )
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 25.w,
+            backgroundColor: AppColors.swatch.shade400,
+          ),
+          10.horizontalSpace,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Jaber Khalaf',
+                style: TextStyle(fontSize: 17.sp),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Icon(
+                    Icons.location_pin,
+                    color: AppColors.secondary,
+                    size: 18.sp,
+                  ),
+                  Text(
+                    'Tulkarm',
+                    style: TextStyle(
+                      fontSize: 12.sp,
+                      color: Colors.black87,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const Spacer(),
+          Container(
+            height: 46.h,
+            width: 46.h,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.r),
+              color: const Color(0xFF826DEE),
+            ),
+            child: Text(
+              '5km',
+              style: TextStyle(
+                fontSize: 12.sp,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
