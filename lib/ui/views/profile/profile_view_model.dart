155b0f6bcd177638c07116754c749f7af8121481
David Michael II <knowdavidmichael@gmail.com>
2022-05-23 21:45:50 +0100

    feat: add user to profile view model

--- lib/ui/views/profile/profile_view_model.dart
+++ lib/ui/views/profile/profile_view_model.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:stacked/stacked.dart';
 
 class ProfileViewModel extends BaseViewModel {
   Future<void> init() async {}
+
+  final User user = FirebaseAuth.instance.currentUser!;
 }
