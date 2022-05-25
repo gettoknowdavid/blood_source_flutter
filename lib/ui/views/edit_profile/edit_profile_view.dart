6bc930e6788d94255ce5ae7cc14758a91c84e6b8
David Michael II <knowdavidmichael@gmail.com>
2022-05-25 10:56:15 +0100

    feat: update edit profile view

--- lib/ui/views/edit_profile/edit_profile_view.dart
+++ lib/ui/views/edit_profile/edit_profile_view.dart
@@ -1,5 +1,10 @@
+import 'package:blood_source/common/app_colors.dart';
+import 'package:blood_source/ui/shared/widgets/app_back_button.dart';
+import 'package:blood_source/ui/shared/widgets/profile/avatar.dart';
+import 'package:blood_source/ui/shared/widgets/profile_header_paint.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import './edit_profile_view_model.dart';
 
@@ -12,11 +17,37 @@ class EditProfileView extends StatelessWidget {
       viewModelBuilder: () => EditProfileViewModel(),
       onModelReady: (model) async => await model.init(),
       builder: (context, model, Widget? child) {
-        return  Scaffold(
-          body: Center(
-            child: Text(
-              'EditProfileView',
-            ),
+        return Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: AppColors.primary,
+            leading: const AppBackButton(),
+            actions: [
+              TextButton(
+                onPressed: () {},
+                child: const Text('Save'),
+              ),
+            ],
+          ),
+          body: Stack(
+            children: [
+              CustomPaint(
+                child: SizedBox(height: 1.sh, width: 1.sw),
+                painter: ProfileHeaderPainter(),
+              ),
+              SingleChildScrollView(
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      100.verticalSpace,
+                      const Avatar(),
+                      10.verticalSpace,
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         );
       },
