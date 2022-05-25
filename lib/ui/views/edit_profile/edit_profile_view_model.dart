1196ff55044eb7b93e4a2b13dab369d92f16a3a7
David Michael II <knowdavidmichael@gmail.com>
2022-05-25 17:26:59 +0100

    feat: add image picker to edit profile model

--- lib/ui/views/edit_profile/edit_profile_view_model.dart
+++ lib/ui/views/edit_profile/edit_profile_view_model.dart
@@ -1,10 +1,53 @@
+import 'dart:io';
+
+import 'package:blood_source/app/app.locator.dart';
+import 'package:blood_source/services/auth_service.dart';
+import 'package:blood_source/services/media_service.dart';
+import 'package:blood_source/services/storage_service.dart';
+import 'package:blood_source/models/blood_source_user.dart';
+import 'package:blood_source/services/store_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:stacked/stacked.dart';
 
-class EditProfileViewModel extends FutureViewModel {
-  Future<void> init() async {}
+class EditProfileViewModel extends FutureViewModel<BloodSourceUser>
+    with ReactiveServiceMixin {
+  EditProfileViewModel() {
+    listenToReactiveValues([_image]);
+  }
+  final StoreService _storeService = locator<StoreService>();
+  final StorageService _storageService = locator<StorageService>();
+  final AuthService _authService = locator<AuthService>();
+  final MediaService _mediaService = locator<MediaService>();
+
+  final ReactiveValue<File?> _image = ReactiveValue<File?>(null);
+  File? get image => _image.value;
+
+  void getImage() async {
+    final _pickedFile = await _mediaService.getImage(fromGallery: true);
+
+    if (_pickedFile != null) {
+      _image.value = File(_pickedFile.path);
+    } else {
+      print('No image selected.');
+    }
+
+    notifyListeners();
+  }
 
   @override
-  Future futureToRun() {
-    throw UnimplementedError();
+  void dispose() {
+    super.dispose();
   }
+
+  Future<void> init() async {}
+
+  Future<BloodSourceUser> getProfile() async {
+    final result = await _storeService.getUser(
+      FirebaseAuth.instance.currentUser!.uid,
+    );
+    return result!.bSUser!;
+  }
+
+  @override
+  Future<BloodSourceUser> futureToRun() async => await getProfile();
 }
