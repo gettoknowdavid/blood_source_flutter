a8b0e498a02e5db9bc8bbff4494d0f4d467ab3bf
David Michael II <knowdavidmichael@gmail.com>
2022-05-25 09:28:14 +0100

    feat: update splash_view model

--- lib/ui/views/splash/splash_view_model.dart
+++ lib/ui/views/splash/splash_view_model.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:blood_source/app/app.locator.dart';
 import 'package:blood_source/app/app.router.dart';
-import 'package:blood_source/models/custom_user.dart';
+import 'package:blood_source/models/blood_source_user.dart';
 import 'package:blood_source/models/user-type.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -14,8 +14,21 @@ class SplashViewModel extends StreamViewModel<User?> {
   Timer? timer;
 
   NavigationService navService = locator<NavigationService>();
+  // StorageService storageService = locator<StorageService>();
   FirebaseAuthenticationService authService =
       locator<FirebaseAuthenticationService>();
+
+  bool hasAccess() {
+    bool access = false;
+    FirebaseAuth.instance.authStateChanges().listen((event) {
+      if (event != null) {
+        access = true;
+      } else {
+        access = false;
+      }
+    });
+    return access;
+  }
 
   bool isVerified() {
     return authService.currentUser!.emailVerified;
@@ -25,48 +38,47 @@ class SplashViewModel extends StreamViewModel<User?> {
   Stream<User?> get stream => authService.authStateChanges;
 
   Future checkVerification() async {
-    FirebaseAuth.instance.authStateChanges().listen((User? user) async {
-      if (user == null) {
-        timer?.cancel();
-        navService.clearStackAndShow(Routes.signInView);
+    await FirebaseAuth.instance.currentUser!.reload();
+
+    if (!isVerified()) {
+      navService.clearStackAndShow(Routes.verifyEmailView);
+    }
+
+    if (isVerified()) {
+      final authUser = FirebaseAuth.instance.currentUser!;
+      final _ref = FirebaseFirestore.instance
+          .collection('users')
+          .doc(authUser.uid)
+          .withConverter<BloodSourceUser>(
+              fromFirestore: BloodSourceUser.fromFirestore,
+              toFirestore: (_cs, _) => _cs.toFirestore());
+
+      final _docSnap = await _ref.get();
+      final _user = _docSnap.data();
+
+      if (_user!.userType == UserType.donor && !_user.isDonorFormComplete) {
+        navService.clearStackAndShow(Routes.donorFormView);
+        notifyListeners();
       }
 
-      if (user != null) {
-        timer?.cancel();
-        await FirebaseAuth.instance.currentUser!.reload();
+      navService.clearStackAndShow(Routes.appLayoutView);
+      notifyListeners();
+    }
+  }
 
-        if (!isVerified()) {
-          navService.clearStackAndShow(Routes.verifyEmailView);
-        }
-
-        if (isVerified()) {
-          final user = FirebaseAuth.instance.currentUser!.uid;
-          final _ref = FirebaseFirestore.instance
-              .collection('users')
-              .doc(user)
-              .withConverter<CustomUser>(
-                  fromFirestore: CustomUser.fromFirestore,
-                  toFirestore: (_cs, _) => _cs.toFirestore());
-
-          final _docSnap = await _ref.get();
-          final _cUser = _docSnap.data();
-
-          if (_cUser!.userType == UserType.donor &&
-              !_cUser.isDonorFormComplete) {
-            navService.clearStackAndShow(Routes.donorFormView);
-            notifyListeners();
-          }
-
-          navService.clearStackAndShow(Routes.appLayoutView);
-          notifyListeners();
-        }
+  Future<void> init() async {
+    Future.delayed(const Duration(seconds: 5)).then((_) {
+      if (FirebaseAuth.instance.currentUser != null) {
+        navService.clearStackAndShow(Routes.appLayoutView);
+      } else {
+        navService.clearStackAndShow(Routes.signInView);
       }
     });
   }
 
-  Future<void> init() async {
-    timer = Timer.periodic(const Duration(seconds: 5), (_) async {
-      await checkVerification();
-    });
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
   }
 }
