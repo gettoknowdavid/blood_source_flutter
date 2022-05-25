d7ab5d4648661d4ec8281121c547791e6097d969
David Michael II <knowdavidmichael@gmail.com>
2022-05-25 09:28:28 +0100

    feat: update verify_email view

--- lib/ui/views/verify_email/verify_email_view.dart
+++ lib/ui/views/verify_email/verify_email_view.dart
@@ -1,4 +1,3 @@
-import 'package:blood_source/app/app.router.dart';
 import 'package:blood_source/common/app_colors.dart';
 import 'package:blood_source/common/image_resources.dart';
 import 'package:blood_source/ui/layouts/auth_layout.dart';
@@ -21,54 +20,48 @@ class VerifyEmailView extends StatelessWidget {
       viewModelBuilder: () => VerifyEmailViewModel(),
       onModelReady: (model) async => await model.init(),
       builder: (context, model, Widget? child) {
-        if (model.isEmailVerified) {
-          return const HomeView();
-        } else {
-          return AuthLayout(
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  const AuthLayoutHeader(
-                    title: 'Email Verification',
-                    subtitle:
-                        "A verification email has been sent to your email, please verify your account.",
-                  ),
-                  10.verticalSpace,
-                  Image.asset(ImageResources.verifyEmail),
-                  18.verticalSpace,
-                  AppButton(
-                    onTap: () => model.openMailApp(),
-                    text: model.isEmailVerified ? 'Continue' : 'Open email app',
-                  ),
-                  20.verticalSpace,
-                  model.isEmailVerified
-                      ? const SizedBox()
-                      : Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            AppTextButton(
-                              onTap: () => model.resendEmailVerification(),
-                              text: 'Resend Email',
-                              fontSize: 14.sp,
-                              color: AppColors.swatch.shade900,
-                              fontWeight: FontWeight.w600,
-                            ),
-                            AppTextButton(
-                              onTap: () => model.cancelVErification(),
-                              text: 'Cancel',
-                              fontSize: 14.sp,
-                              color: AppColors.swatch.shade900,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ],
-                        ),
-                ],
-              ),
+        return AuthLayout(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const AuthLayoutHeader(
+                  title: 'Email Verification',
+                  subtitle:
+                      "A verification email has been sent to your email, please verify your account.",
+                ),
+                10.verticalSpace,
+                Image.asset(ImageResources.verifyEmail),
+                18.verticalSpace,
+                AppButton(
+                  onTap: () => model.openMailApp(),
+                  text: 'Open email app',
+                ),
+                20.verticalSpace,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    AppTextButton(
+                      onTap: () => model.resendEmailVerification(),
+                      text: 'Resend Email',
+                      fontSize: 14.sp,
+                      color: AppColors.swatch.shade900,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    AppTextButton(
+                      onTap: () => model.cancelVErification(),
+                      text: 'Cancel',
+                      fontSize: 14.sp,
+                      color: AppColors.swatch.shade900,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ],
+                ),
+              ],
             ),
-          );
-        }
+          ),
+        );
       },
     );
   }
