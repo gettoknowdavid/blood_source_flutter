1df87fbfecf13979925342c2c89485964e1a2860
David Michael II <knowdavidmichael@gmail.com>
2022-05-25 07:18:35 +0100

    feat: update blood_group enum

--- lib/models/blood_group.dart
+++ lib/models/blood_group.dart
@@ -1,4 +1,5 @@
 enum BloodGroup {
+  none,
   aPositive,
   aNegative,
   bPositive,
@@ -20,23 +21,37 @@ extension BloodGroupEx on BloodGroup {
   BloodGroupExType get value {
     switch (this) {
       case BloodGroup.aPositive:
-        return BloodGroupExType(name: 'A+', desc: 'A Rhd Positive (A+)');
+        return BloodGroupExType(name: 'A+', desc: 'A Positive (A+)');
       case BloodGroup.aNegative:
-        return BloodGroupExType(name: 'A-', desc: 'A Rhd Negative (A-)');
+        return BloodGroupExType(name: 'A-', desc: 'A Negative (A-)');
       case BloodGroup.bPositive:
-        return BloodGroupExType(name: 'B+', desc: 'B Rhd Positive (B+)');
+        return BloodGroupExType(name: 'B+', desc: 'B Positive (B+)');
       case BloodGroup.bNegative:
-        return BloodGroupExType(name: 'B-', desc: 'B Rhd Negative (B-)');
+        return BloodGroupExType(name: 'B-', desc: 'B Negative (B-)');
       case BloodGroup.oPositive:
-        return BloodGroupExType(name: 'O+', desc: 'O Rhd Positive (O+)');
+        return BloodGroupExType(name: 'O+', desc: 'O Positive (O+)');
       case BloodGroup.oNegative:
-        return BloodGroupExType(name: 'O-', desc: 'O Rhd Negative (O-)');
+        return BloodGroupExType(name: 'O-', desc: 'O Negative (O-)');
       case BloodGroup.abPositive:
-        return BloodGroupExType(name: 'AB+', desc: 'AB Rhd Positive (AB+)');
+        return BloodGroupExType(name: 'AB+', desc: 'AB Positive (AB+)');
       case BloodGroup.abNegative:
-        return BloodGroupExType(name: 'AB-', desc: 'AB Rhd Negative (AB-)');
+        return BloodGroupExType(name: 'AB-', desc: 'AB Negative (AB-)');
       default:
         return BloodGroupExType(name: '', desc: '');
     }
   }
 }
+
+const $BloodGroupTypeEnum = {
+  BloodGroup.none: 'none',
+  BloodGroup.aPositive: 'A Positive (A+)',
+  BloodGroup.aNegative: 'A Negative (A-)',
+  BloodGroup.bPositive: 'B Positive (B+)',
+  BloodGroup.bNegative: 'B Negative (B-)',
+  BloodGroup.oPositive: 'O Positive (O+)',
+  BloodGroup.oNegative: 'O Negative (O-)',
+  BloodGroup.abPositive: 'AB Positive (AB+)',
+  BloodGroup.abNegative: 'AB Negative (AB-)',
+};
+
+
