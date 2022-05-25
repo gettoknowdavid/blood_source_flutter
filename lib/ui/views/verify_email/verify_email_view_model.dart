6970c8cc95d9a787f858bf7a9851899f11f2634c
David Michael II <knowdavidmichael@gmail.com>
2022-05-25 09:30:04 +0100

    feat: update verify_email_view model

--- lib/ui/views/verify_email/verify_email_view_model.dart
+++ lib/ui/views/verify_email/verify_email_view_model.dart
@@ -1,13 +1,18 @@
 import 'dart:async';
 
 import 'package:blood_source/app/app.locator.dart';
+import 'package:blood_source/app/app.router.dart';
 import 'package:blood_source/common/app_colors.dart';
+import 'package:blood_source/models/user-type.dart';
+import 'package:blood_source/services/storage_service.dart';
 import 'package:blood_source/utils/dialog_type.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:open_mail_app/open_mail_app.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_firebase_auth/stacked_firebase_auth.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:blood_source/models/blood_source_user.dart';
 
 class VerifyEmailViewModel extends BaseViewModel with ReactiveServiceMixin {
   VerifyEmailViewModel() {
@@ -16,6 +21,7 @@ class VerifyEmailViewModel extends BaseViewModel with ReactiveServiceMixin {
   final ReactiveValue<bool> _isEmailVerified = ReactiveValue<bool>(false);
   bool get isEmailVerified => _isEmailVerified.value;
 
+  StorageService storageService = locator<StorageService>();
   DialogService dialogService = locator<DialogService>();
   NavigationService navService = locator<NavigationService>();
   FirebaseAuthenticationService authService =
@@ -39,13 +45,46 @@ class VerifyEmailViewModel extends BaseViewModel with ReactiveServiceMixin {
 
   Future cancelVErification() async {
     await FirebaseAuth.instance.signOut();
-    navService.popUntil((route) => route.isFirst);
+    navService.clearStackAndShow(Routes.signInView);
     notifyListeners();
   }
 
   Future checkVerification() async {
-    timer?.cancel();
     await FirebaseAuth.instance.currentUser!.reload();
+    _isEmailVerified.value = FirebaseAuth.instance.currentUser!.emailVerified;
+
+    if (_isEmailVerified.value == true) {
+      timer?.cancel();
+
+      final authUser = FirebaseAuth.instance.currentUser!;
+      final _ref = FirebaseFirestore.instance
+          .collection('users')
+          .doc(authUser.uid)
+          .withConverter<BloodSourceUser>(
+              fromFirestore: BloodSourceUser.fromFirestore,
+              toFirestore: (_cs, _) => _cs.toFirestore());
+
+      final _docSnap = await _ref.get();
+      final _user = _docSnap.data();
+
+      storageService.saveToDisk(authUser.uid, _user!.toJson());
+
+      if (_user.userType == UserType.donor && !_user.isDonorFormComplete) {
+        navService.clearStackAndShow(Routes.donorFormView);
+        notifyListeners();
+      }
+
+      if (_user.userType == UserType.donor && _user.isDonorFormComplete) {
+        navService.clearStackAndShow(Routes.appLayoutView);
+        notifyListeners();
+      }
+
+      if (_user.userType == UserType.recipient) {
+        navService.clearStackAndShow(Routes.appLayoutView);
+        notifyListeners();
+      }
+    }
+
     notifyListeners();
   }
 
