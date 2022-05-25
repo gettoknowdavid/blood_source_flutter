133eaa0e14457f694f039f0c5f8cd32d8e8c42de
David Michael II <knowdavidmichael@gmail.com>
2022-05-25 07:21:07 +0100

    feat: remove app_bar from app_layout

--- lib/ui/layouts/app_layout/app_layout_view.dart
+++ lib/ui/layouts/app_layout/app_layout_view.dart
@@ -2,7 +2,6 @@ import 'package:animations/animations.dart';
 import 'package:blood_source/common/app_colors.dart';
 import 'package:blood_source/common/app_icons.dart';
 import 'package:blood_source/ui/views/dashboard/dashboard_view.dart';
-import 'package:blood_source/ui/views/donate/donate_view.dart';
 import 'package:blood_source/ui/views/notifications/notifications_view.dart';
 import 'package:blood_source/ui/views/profile/profile_view.dart';
 import 'package:flutter/material.dart';
@@ -21,7 +20,7 @@ class AppLayoutView extends StatelessWidget {
         case 0:
           return const DashboardView();
         case 1:
-          return const DonateView();
+          return const ProfileView();
         case 2:
           return const NotificationsView();
         case 3:
@@ -35,47 +34,6 @@ class AppLayoutView extends StatelessWidget {
       viewModelBuilder: () => AppLayoutViewModel(),
       builder: (context, model, Widget? child) {
         return Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.white,
-            elevation: 0,
-            leadingWidth: 100.w,
-            leading: Container(
-              alignment: Alignment.centerLeft,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        height: 42.h,
-                        width: 42.h,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          color: AppColors.swatch.shade100,
-                          borderRadius: BorderRadius.circular(12.r),
-                        ),
-                        child: Icon(
-                          Icons.pin_drop_outlined,
-                          size: 26.sp,
-                          color: AppColors.primaryDark,
-                        ),
-                      ),
-                      4.horizontalSpace,
-                      Text(
-                        'PHC',
-                        style: TextStyle(
-                          fontSize: 14.sp,
-                          color: AppColors.primary,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
           body: SafeArea(
             child: PageTransitionSwitcher(
               duration: const Duration(milliseconds: 300),
