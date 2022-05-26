dffed2f011becadc268857c7e6019d10f4af6eb4
David Michael II <knowdavidmichael@gmail.com>
2022-05-26 14:08:13 +0100

    feat: add location service t dashboard

--- lib/ui/views/dashboard/dashboard_view_model.dart
+++ lib/ui/views/dashboard/dashboard_view_model.dart
@@ -3,9 +3,22 @@ import 'package:blood_source/services/location_service.dart';
 import 'package:stacked/stacked.dart';
 
 class DashboardViewModel extends ReactiveViewModel {
-  Future<void> init() async {}
+  Future longUpdateStuff() async {
+    // Sets busy to true before starting future and sets it to false after executing
+    // You can also pass in an object as the busy object. Otherwise it'll use the ViewModel
+    var result = await runBusyFuture(getLocation());
+    return result;
+  }
+
+  Future<void> init() async {
+    await longUpdateStuff();
+  }
 
   LocationService locationService = locator<LocationService>();
+
+  Future getLocation() async {
+    await locationService.getPlace();
+  }
 
   @override
   List<ReactiveServiceMixin> get reactiveServices => [locationService];
