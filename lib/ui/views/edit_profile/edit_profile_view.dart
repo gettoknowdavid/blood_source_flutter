1495b3bdeded54f346989ea5847380dd98e397be
David Michael II <knowdavidmichael@gmail.com>
2022-05-26 08:16:26 +0100

    fix: remove app bar from edit profile view

--- lib/ui/views/edit_profile/edit_profile_view.dart
+++ lib/ui/views/edit_profile/edit_profile_view.dart
@@ -2,9 +2,8 @@ import 'package:blood_source/common/app_colors.dart';
 import 'package:blood_source/models/blood_group.dart';
 import 'package:blood_source/models/gender.dart';
 import 'package:blood_source/ui/shared/widgets/app_back_button.dart';
+import 'package:blood_source/ui/shared/widgets/app_text_button.dart';
 import 'package:blood_source/ui/shared/widgets/app_textfield.dart';
-import 'package:blood_source/ui/shared/widgets/loading_indicator.dart';
-import 'package:blood_source/ui/shared/widgets/profile/avatar.dart';
 import 'package:blood_source/ui/shared/widgets/profile_header_paint.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
@@ -25,23 +24,6 @@ class EditProfileView extends StatelessWidget {
       builder: (context, model, Widget? child) {
         return Scaffold(
           backgroundColor: Colors.white,
-          appBar: AppBar(
-            elevation: 0,
-            backgroundColor: AppColors.primary,
-            leading: const AppBackButton(),
-            actions: [
-              TextButton(
-                onPressed: () => model.save(),
-                child: const Text(
-                  'Save',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ],
-          ),
           body: Stack(
             children: [
               CustomPaint(
@@ -49,123 +31,138 @@ class EditProfileView extends StatelessWidget {
                 painter: ProfileHeaderPainter(),
               ),
               SingleChildScrollView(
-                child: Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      100.verticalSpace,
-                      GestureDetector(
-                        onTap: model.getImage,
+                padding: const EdgeInsets.fromLTRB(16, 32, 16, 32).r,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const AppBackButton(),
+                        AppTextButton(
+                          onTap: () => model.save(),
+                          text: 'Save',
+                          color: Colors.white,
+                          fontSize: 16.r,
+                        ),
+                      ],
+                    ),
+                    0.verticalSpace,
+                    GestureDetector(
+                      onTap: model.getImage,
+                      child: CircleAvatar(
+                        radius: 0.18 * 1.sw,
+                        backgroundColor: Colors.white,
                         child: CircleAvatar(
-                          radius: 0.18 * 1.sw,
-                          backgroundColor: Colors.white,
-                          child: CircleAvatar(
-                            radius: 0.17 * 1.sw,
-                            foregroundColor: AppColors.primary,
-                            foregroundImage: model.image != null
-                                ? FileImage(model.image!)
-                                : null,
-                            backgroundImage: user.avatar != null
-                                ? NetworkImage(user.avatar!)
-                                : null,
-                            child: model.image == null
-                                ? const Icon(Icons.add_a_photo)
-                                : const SizedBox(),
+                          radius: 0.17 * 1.sw,
+                          foregroundColor: AppColors.primary,
+                          foregroundImage: model.image != null
+                              ? FileImage(model.image!)
+                              : null,
+                          backgroundImage: user.avatar != null
+                              ? NetworkImage(user.avatar!)
+                              : null,
+                          child: model.image == null
+                              ? const Icon(Icons.add_a_photo)
+                              : const SizedBox(),
+                        ),
+                      ),
+                    ),
+                    40.verticalSpace,
+                    AppTextField(
+                      controller: model.nameController,
+                      label: 'Name',
+                    ),
+                    15.verticalSpace,
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      padding: EdgeInsets.symmetric(horizontal: 20.r),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Gender', style: TextStyle(fontSize: 11.r)),
+                          DropdownButton<Gender>(
+                            value: model.gender,
+                            borderRadius: BorderRadius.circular(20.r),
+                            icon: const Icon(Icons.keyboard_arrow_down),
+                            style:
+                                TextStyle(fontSize: 16.sp, color: Colors.black),
+                            onChanged: (value) => model.onGenderChanged(value),
+                            items: Gender.values
+                                .map<DropdownMenuItem<Gender>>((e) {
+                              return DropdownMenuItem<Gender>(
+                                value: e,
+                                child: Row(
+                                  children: [
+                                    Text(e.value),
+                                    4.horizontalSpace,
+                                    Icon(model.getGenderIcon(e), size: 20.r),
+                                  ],
+                                ),
+                              );
+                            }).toList(),
                           ),
-                        ),
+                        ],
                       ),
-                      10.verticalSpace,
-                      AppTextField(
-                        controller: model.nameController,
-                        label: 'Name',
-                      ),
-                      15.verticalSpace,
-                      Container(
-                        alignment: Alignment.centerLeft,
-                        padding: EdgeInsets.symmetric(horizontal: 20.r),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Gender', style: TextStyle(fontSize: 11.r)),
-                            DropdownButton<Gender>(
-                              value: model.gender,
-                              borderRadius: BorderRadius.circular(20.r),
-                              icon: const Icon(Icons.keyboard_arrow_down),
-                              style: TextStyle(
-                                  fontSize: 16.sp, color: Colors.black),
-                              onChanged: (value) =>
-                                  model.onGenderChanged(value),
-                              items: Gender.values
-                                  .map<DropdownMenuItem<Gender>>((e) {
-                                return DropdownMenuItem<Gender>(
-                                  value: e,
-                                  child: Text(e.value),
-                                );
-                              }).toList(),
+                    ),
+                    25.verticalSpace,
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      padding: EdgeInsets.symmetric(horizontal: 20.r),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Blood Group', style: TextStyle(fontSize: 11.r)),
+                          DropdownButton<BloodGroup>(
+                            value: model.bloodType,
+                            borderRadius: BorderRadius.circular(20.r),
+                            icon: const Icon(Icons.keyboard_arrow_down),
+                            style: TextStyle(
+                              fontSize: 16.sp,
+                              color: Colors.black87,
                             ),
-                          ],
-                        ),
+                            onChanged: (value) =>
+                                model.onBloodGroupChanged(value),
+                            underline:
+                                Container(height: 1.h, color: Colors.grey),
+                            itemHeight: 56.h,
+                            items: BloodGroup.values
+                                .map<DropdownMenuItem<BloodGroup>>((e) {
+                              return DropdownMenuItem<BloodGroup>(
+                                value: e,
+                                child: Text(e.value.desc),
+                              );
+                            }).toList(),
+                          )
+                        ],
                       ),
-                      25.verticalSpace,
-                      Container(
-                        alignment: Alignment.centerLeft,
-                        padding: EdgeInsets.symmetric(horizontal: 20.r),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Blood Group',
-                                style: TextStyle(fontSize: 11.r)),
-                            DropdownButton<BloodGroup>(
-                              value: model.bloodType,
-                              borderRadius: BorderRadius.circular(20.r),
-                              icon: const Icon(Icons.keyboard_arrow_down),
-                              style: TextStyle(
-                                fontSize: 16.sp,
-                                color: Colors.black87,
-                              ),
-                              onChanged: (value) =>
-                                  model.onBloodGroupChanged(value),
-                              underline:
-                                  Container(height: 1.h, color: Colors.grey),
-                              itemHeight: 56.h,
-                              items: BloodGroup.values
-                                  .map<DropdownMenuItem<BloodGroup>>((e) {
-                                return DropdownMenuItem<BloodGroup>(
-                                  value: e,
-                                  child: Text(e.value.desc),
-                                );
-                              }).toList(),
-                            )
-                          ],
-                        ),
-                      ),
-                      30.verticalSpace,
-                      AppTextField(
-                        controller: model.ageController,
-                        label: 'Age',
-                        keyboardType: TextInputType.number,
-                      ),
-                      15.verticalSpace,
-                      AppTextField(
-                        controller: model.phoneController,
-                        label: 'Phone',
-                        hintText: 'Phone',
-                        keyboardType: TextInputType.phone,
-                      ),
-                      15.verticalSpace,
-                      AppTextField(
-                        controller: model.weightController,
-                        label: 'Weight',
-                        keyboardType: TextInputType.number,
-                      ),
-                      15.verticalSpace,
-                      AppTextField(
-                        controller: model.heightController,
-                        label: 'Height',
-                        keyboardType: TextInputType.number,
-                      ),
-                    ],
-                  ),
+                    ),
+                    30.verticalSpace,
+                    AppTextField(
+                      controller: model.ageController,
+                      label: 'Age',
+                      keyboardType: TextInputType.number,
+                    ),
+                    15.verticalSpace,
+                    AppTextField(
+                      controller: model.phoneController,
+                      label: 'Phone',
+                      hintText: 'Phone',
+                      keyboardType: TextInputType.phone,
+                    ),
+                    15.verticalSpace,
+                    AppTextField(
+                      controller: model.weightController,
+                      label: 'Weight',
+                      keyboardType: TextInputType.number,
+                    ),
+                    15.verticalSpace,
+                    AppTextField(
+                      controller: model.heightController,
+                      label: 'Height',
+                      keyboardType: TextInputType.number,
+                    ),
+                  ],
                 ),
               ),
             ],
