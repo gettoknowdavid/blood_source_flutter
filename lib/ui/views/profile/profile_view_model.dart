1e048e5206972cb9b468c1a67851cb8597ad106d
David Michael II <knowdavidmichael@gmail.com>
2022-05-26 14:12:37 +0100

    feat: update profile view

--- lib/ui/views/profile/profile_view_model.dart
+++ lib/ui/views/profile/profile_view_model.dart
@@ -2,23 +2,18 @@ import 'dart:async';
 
 import 'package:blood_source/app/app.locator.dart';
 import 'package:blood_source/app/app.router.dart';
-import 'package:blood_source/models/blood_group.dart';
 import 'package:blood_source/models/blood_source_user.dart';
 import 'package:blood_source/services/auth_service.dart';
-import 'package:blood_source/services/storage_service.dart';
 import 'package:blood_source/services/store_service.dart';
 import 'package:blood_source/ui/views/edit_profile/edit_profile_view.dart';
 import 'package:blood_source/utils/dialog_type.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
-import 'package:logger/logger.dart';
 
 class ProfileViewModel extends ReactiveViewModel {
-  final Logger _logger = Logger();
   final DialogService _dialogService = locator<DialogService>();
   final NavigationService _navService = locator<NavigationService>();
-  final StorageService _storageService = locator<StorageService>();
   final AuthService _authService = locator<AuthService>();
   final StoreService _storeService = locator<StoreService>();
 
