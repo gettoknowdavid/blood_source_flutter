a5657cf5cadbf44d4a131eb24f667268d8601c12
David Michael II <knowdavidmichael@gmail.com>
2022-05-26 12:51:45 +0100

    feat: add location service to dashboard view model

--- lib/ui/views/dashboard/dashboard_view_model.dart
+++ lib/ui/views/dashboard/dashboard_view_model.dart
@@ -1,5 +1,12 @@
+import 'package:blood_source/app/app.locator.dart';
+import 'package:blood_source/services/location_service.dart';
 import 'package:stacked/stacked.dart';
 
-class DashboardViewModel extends BaseViewModel {
+class DashboardViewModel extends ReactiveViewModel {
   Future<void> init() async {}
+
+  LocationService locationService = locator<LocationService>();
+
+  @override
+  List<ReactiveServiceMixin> get reactiveServices => [locationService];
 }
