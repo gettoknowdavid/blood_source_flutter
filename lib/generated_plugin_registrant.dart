6358383c3ec66455f7e732671d6f8c52a8e33d85
David Michael II <knowdavidmichael@gmail.com>
2022-05-26 09:27:15 +0100

    chore: add location plugin

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -14,6 +14,7 @@ import 'package:firebase_storage_web/firebase_storage_web.dart';
 import 'package:geolocator_web/geolocator_web.dart';
 import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:location_web/location_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:sign_in_with_apple_web/sign_in_with_apple_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
@@ -30,6 +31,7 @@ void registerPlugins(Registrar registrar) {
   GeolocatorPlugin.registerWith(registrar);
   GoogleSignInPlugin.registerWith(registrar);
   ImagePickerPlugin.registerWith(registrar);
+  LocationWebPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   SignInWithApplePlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
