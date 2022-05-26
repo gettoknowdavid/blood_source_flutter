2fde9b6a7d39149fca7879d50ac7ed9f65d810f9
David Michael II <knowdavidmichael@gmail.com>
2022-05-26 01:16:16 +0100

    feat: add reactivity to store_service

--- lib/services/store_service.dart
+++ lib/services/store_service.dart
@@ -1,10 +1,18 @@
 import 'package:blood_source/app/app.locator.dart';
-import 'package:blood_source/common/storage_keys.dart';
 import 'package:blood_source/services/storage_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:blood_source/models/blood_source_user.dart';
+import 'package:stacked/stacked.dart';
 
-class StoreService {
+class StoreService with ReactiveServiceMixin {
+  StoreService() {
+    listenToReactiveValues([_bloodUser]);
+  }
+
+  final ReactiveValue<BloodSourceUser?> _bloodUser =
+      ReactiveValue<BloodSourceUser?>(null);
+  BloodSourceUser? get bloodUser => _bloodUser.value;
+
   final StorageService _storageService = locator<StorageService>();
 
   final _usersColRef = FirebaseFirestore.instance.collection('users');
@@ -13,6 +21,7 @@ class StoreService {
     try {
       await _usersColRef.doc(user.uid).set(user.toFirestore());
       _storageService.saveToDisk(user.uid!, user.toJson());
+      _bloodUser.value = user;
       return StoreResult(bSUser: user);
     } on FirebaseException catch (e) {
       return StoreResult.error(errorMessage: e.message);
@@ -23,6 +32,7 @@ class StoreService {
     try {
       await _usersColRef.doc(user.uid).update(user.toFirestore());
       _storageService.saveToDisk(user.uid!, user.toJson());
+      _bloodUser.value = user;
       return StoreResult(bSUser: user);
     } on FirebaseException catch (e) {
       return StoreResult.error(errorMessage: e.message);
@@ -37,6 +47,7 @@ class StoreService {
               fromFirestore: BloodSourceUser.fromFirestore,
               toFirestore: (_u, _) => _u.toFirestore())
           .get();
+      _bloodUser.value = _userData.data();
       return StoreResult(bSUser: _userData.data());
     } on FirebaseException catch (e) {
       return StoreResult.error(errorMessage: e.message);
@@ -46,6 +57,7 @@ class StoreService {
   Future<StoreResult?> getUserFromLocalStorage(String uid) async {
     try {
       final _result = _storageService.getFromDisk(uid);
+      _bloodUser.value = _result;
       return StoreResult(bSUser: _result);
     } on FirebaseException catch (e) {
       return StoreResult.error(errorMessage: e.message);
