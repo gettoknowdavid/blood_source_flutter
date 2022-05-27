c854508de0264a10b04a9932bf42bab747dfb53b
David Michael II <knowdavidmichael@gmail.com>
2022-05-27 08:33:58 +0100

    feat: add dynamic dashboard button list

--- lib/ui/views/dashboard/dashboard_view_model.dart
+++ lib/ui/views/dashboard/dashboard_view_model.dart
@@ -1,21 +1,47 @@
 import 'package:blood_source/app/app.locator.dart';
+import 'package:blood_source/models/dashboard_button_model.dart';
+import 'package:blood_source/models/user-type.dart';
 import 'package:blood_source/services/auth_service.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:blood_source/services/store_service.dart';
+import 'package:blood_source/models/blood_source_user.dart';
 import 'package:stacked/stacked.dart';
 
-class DashboardViewModel extends ReactiveViewModel {
+class DashboardViewModel extends ReactiveViewModel with ReactiveServiceMixin {
   Future longUpdateStuff() async {
-    // var result = await runBusyFuture();
-    // return result;
+    var result = await runBusyFuture(getList());
+    _buttonList.value = result;
   }
 
   Future<void> init() async {}
 
-  final AuthService _authService = locator<AuthService>();
-  User get user => _authService.currentUser!;
+  final ReactiveValue<List<DashboardButtonModel>> _buttonList =
+      ReactiveValue<List<DashboardButtonModel>>(donorButtonList);
+  List<DashboardButtonModel> get buttonList => _buttonList.value;
 
-  String get firstName => user.displayName.toString().split(" ").first;
+  final AuthService _authService = locator<AuthService>();
+  String get displayName => _authService.currentUser!.displayName!;
+
+  final StoreService _storeService = locator<StoreService>();
+  BloodSourceUser? get user => _storeService.bloodUser!;
+
+  String get firstName => displayName.toString().split(" ").first;
+
+  Future<List<DashboardButtonModel>> getList() async {
+    if (user != null) {
+      switch (user!.userType) {
+        case UserType.donor:
+          _buttonList.value = donorButtonList;
+          break;
+        case UserType.recipient:
+          _buttonList.value = recipientButtonList;
+          break;
+        default:
+          _buttonList.value = donorButtonList;
+      }
+    }
+    return _buttonList.value;
+  }
 
   @override
-  List<ReactiveServiceMixin> get reactiveServices => [];
+  List<ReactiveServiceMixin> get reactiveServices => [_storeService];
 }
