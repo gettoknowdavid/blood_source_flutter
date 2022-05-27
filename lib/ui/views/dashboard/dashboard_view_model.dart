3366d09dfa604dc5c4f0dc29999e2747d3615785
David Michael II <knowdavidmichael@gmail.com>
2022-05-27 07:34:56 +0100

    feat: update dahsboad view model

--- lib/ui/views/dashboard/dashboard_view_model.dart
+++ lib/ui/views/dashboard/dashboard_view_model.dart
@@ -1,25 +1,21 @@
 import 'package:blood_source/app/app.locator.dart';
-import 'package:blood_source/services/location_service.dart';
+import 'package:blood_source/services/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:stacked/stacked.dart';
 
 class DashboardViewModel extends ReactiveViewModel {
   Future longUpdateStuff() async {
-    // Sets busy to true before starting future and sets it to false after executing
-    // You can also pass in an object as the busy object. Otherwise it'll use the ViewModel
-    var result = await runBusyFuture(getLocation());
-    return result;
+    // var result = await runBusyFuture();
+    // return result;
   }
 
-  Future<void> init() async {
-    await longUpdateStuff();
-  }
+  Future<void> init() async {}
 
-  LocationService locationService = locator<LocationService>();
+  final AuthService _authService = locator<AuthService>();
+  User get user => _authService.currentUser!;
 
-  Future getLocation() async {
-    await locationService.getPlace();
-  }
+  String get firstName => user.displayName.toString().split(" ").first;
 
   @override
-  List<ReactiveServiceMixin> get reactiveServices => [locationService];
+  List<ReactiveServiceMixin> get reactiveServices => [];
 }
