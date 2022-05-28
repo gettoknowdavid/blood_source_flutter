527bdea5ad56e9a86a2a0ddfda93dfd2664153dd
David Michael II <knowdavidmichael@gmail.com>
2022-05-28 16:36:08 +0100

    feat: add appbar and header photo to request view

--- lib/ui/views/request/request_view.dart
+++ lib/ui/views/request/request_view.dart
@@ -1,5 +1,9 @@
+import 'package:blood_source/common/app_colors.dart';
+import 'package:blood_source/common/image_resources.dart';
+import 'package:blood_source/ui/shared/widgets/app_back_button.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import './request_view_model.dart';
 
@@ -18,10 +22,38 @@ class RequestView extends StatelessWidget {
         RequestViewModel model,
         Widget? child,
       ) {
-        return const Scaffold(
-          body: Center(
-            child: Text(
-              'RequestView',
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: AppColors.swatch.shade700,
+            leading: const AppBackButton(),
+            centerTitle: true,
+            elevation: 0,
+            title: Text(
+              'Request Blood',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 22.r,
+              ),
+            ),
+          ),
+          body: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(4, 20, 4, 0).r,
+                    height: 0.35.sh,
+                    width: 1.sw,
+                    decoration: BoxDecoration(
+                      color: AppColors.swatch.shade700,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(40.r),
+                      ),
+                    ),
+                    child: Image.asset(ImageResources.bloodDonation),
+                  ),
+                ],
+              ),
             ),
           ),
         );
