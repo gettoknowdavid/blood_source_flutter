fe63c0fe187a04086d61015dd33527f7dbd337cd
David Michael II <knowdavidmichael@gmail.com>
2022-05-28 15:05:55 +0100

    feat: add naviagtion to request view

--- lib/ui/layouts/app_layout/app_layout_view_model.dart
+++ lib/ui/layouts/app_layout/app_layout_view_model.dart
@@ -1,3 +1,12 @@
+import 'package:blood_source/app/app.locator.dart';
+import 'package:blood_source/app/app.router.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-class AppLayoutViewModel extends IndexTrackingViewModel {}
+class AppLayoutViewModel extends IndexTrackingViewModel {
+  final NavigationService _navService = locator<NavigationService>();
+
+  void goToMakeRequestView() {
+    _navService.navigateTo(Routes.requestView);
+  }
+}
