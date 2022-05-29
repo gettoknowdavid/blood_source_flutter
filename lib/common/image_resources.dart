ebfe18a5d4ddfd12932d47abeb1836371947edbd
David Michael II <knowdavidmichael@gmail.com>
2022-05-29 18:29:24 +0100

    chore: add empty image

--- lib/common/image_resources.dart
+++ lib/common/image_resources.dart
@@ -5,4 +5,5 @@ class ImageResources {
   static const String forgotPassword = "assets/images/forgot-email.png";
   static const String checkEmail = "assets/images/check-email.png";
   static const String bloodDonation = "assets/images/blood-donation.png";
+  static const String empty = "assets/images/empty.png";
 }
