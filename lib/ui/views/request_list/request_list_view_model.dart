66dd9c5a6561e9d74c12411b301929f3eb72d4d9
David Michael II <knowdavidmichael@gmail.com>
2022-05-29 17:11:43 +0100

    feat: listen to store service in request vm

--- lib/ui/views/request_list/request_list_view_model.dart
+++ lib/ui/views/request_list/request_list_view_model.dart
@@ -1,8 +1,12 @@
+import 'package:blood_source/app/app.locator.dart';
+import 'package:blood_source/services/store_service.dart';
 import 'package:stacked/stacked.dart';
 
 class RequestListViewModel extends ReactiveViewModel {
+  final StoreService _storeService = locator<StoreService>();
+
   Future<void> init() async {}
 
   @override
-  List<ReactiveServiceMixin> get reactiveServices => throw UnimplementedError();
+  List<ReactiveServiceMixin> get reactiveServices => [_storeService];
 }
