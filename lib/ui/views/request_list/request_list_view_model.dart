0854178952ed0caea48c3b90ff93d86cdf015661
David Michael II <knowdavidmichael@gmail.com>
2022-05-29 17:20:41 +0100

    feat: request list view model

--- lib/ui/views/request_list/request_list_view_model.dart
+++ lib/ui/views/request_list/request_list_view_model.dart
@@ -1,11 +1,21 @@
 import 'package:blood_source/app/app.locator.dart';
+import 'package:blood_source/models/request.dart';
 import 'package:blood_source/services/store_service.dart';
 import 'package:stacked/stacked.dart';
 
 class RequestListViewModel extends ReactiveViewModel {
   final StoreService _storeService = locator<StoreService>();
 
-  Future<void> init() async {}
+  Future<void> init() async {
+    await longUpdateStuff();
+  }
+
+  Future longUpdateStuff() async {
+    var result = await runBusyFuture(getRequests());
+    return result;
+  }
+
+  Future<List<Request>?> getRequests() async {}
 
   @override
   List<ReactiveServiceMixin> get reactiveServices => [_storeService];
