cea82399685886341fb28a61313b40ffa8296771
David Michael II <knowdavidmichael@gmail.com>
2022-05-30 18:59:27 +0100

    fix: donor view model request

--- lib/ui/views/donor/donor_view_model.dart
+++ lib/ui/views/donor/donor_view_model.dart
@@ -2,14 +2,13 @@ import 'package:blood_source/app/app.locator.dart';
 import 'package:blood_source/models/blood_source_user.dart';
 import 'package:blood_source/models/request.dart';
 import 'package:blood_source/services/store_service.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:logger/logger.dart';
 
 class DonorViewModel extends ReactiveViewModel with ReactiveServiceMixin {
   DonorViewModel() {
-    listenToReactiveValues([_donors, _donorCount]);
+    listenToReactiveValues([_donors]);
   }
 
   final Logger _logger = Logger();
@@ -21,37 +20,47 @@ class DonorViewModel extends ReactiveViewModel with ReactiveServiceMixin {
   final ReactiveValue<List<BloodSourceUser>> _donors =
       ReactiveValue<List<BloodSourceUser>>([]);
   List<BloodSourceUser> get donors => _donors.value;
+
   BloodSourceUser? get user => _storeService.bloodUser;
 
-  final ReactiveValue<int> _donorCount = ReactiveValue<int>(0);
-  int get donorCount => _donorCount.value;
+  Request? get request => _storeService.request;
 
   Future<void> init() async {
-    getDonors();
-    getDonorCountString();
+    await longUpdateStuff();
   }
 
-  String getDonorCountString() {
-    if (_donorCount.value > 1) {
-      return '${_donorCount.value} donor are available';
-    }
+  // String getDonorCountString() {
+  //   if (_donorCount.value > 1) {
+  //     return '${_donorCount.value} donor are available';
+  //   }
 
-    if (_donorCount.value == 1) {
-      return '${_donorCount.value} donors are available';
-    }
+  //   if (_donorCount.value == 1) {
+  //     return '${_donorCount.value} donors are available';
+  //   }
 
-    return 'No donors available';
+  //   return 'No donors available';
+  // }
+
+  Future getDonors() async {
+    // FirebaseFirestore.instance
+    //     .collection('users')
+    //     .where('uid', isNotEqualTo: FirebaseAuth.instance.currentUser!.uid)
+    //     .where('bloodGroup', isEqualTo: request!.bloodGroup.value.desc)
+    //     .where('userType', isEqualTo: UserType.donor.name)
+    //     .snapshots()
+    //     .listen((event) {
+    //   _donors.value = event.docs
+    //       .map((e) => BloodSourceUser.fromFirestore(e, null))
+    //       .toList();
+
+    // });
+
+    final result = await _storeService.getDonors();
+    _donors.value = result.donors!;
   }
 
-  List<BloodSourceUser> getDonors() {
-    FirebaseFirestore.instance.collection("donors").snapshots().listen((event) {
-      for (var doc in event.docs) {
-        _donors.value.add(BloodSourceUser.fromFirestore(doc, null));
-      }
-      _donorCount.value = _donors.value.length;
-    });
-
-    return _donors.value;
+  Future longUpdateStuff() async {
+    await runBusyFuture(getDonors());
   }
 
   Future addRequest(Request request) async {
