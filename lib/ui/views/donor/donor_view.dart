f3858a6ef1981ad0479feb11aa7e44a135d98a0c
David Michael II <knowdavidmichael@gmail.com>
2022-05-30 19:00:01 +0100

    feat: update donor view  loading and empty widget

--- lib/ui/views/donor/donor_view.dart
+++ lib/ui/views/donor/donor_view.dart
@@ -4,6 +4,8 @@ import 'package:blood_source/models/request.dart';
 import 'package:blood_source/ui/shared/widgets/app_back_button.dart';
 import 'package:blood_source/ui/shared/widgets/app_text_button.dart';
 import 'package:blood_source/ui/shared/widgets/donor/donor_list_item.dart';
+import 'package:blood_source/ui/shared/widgets/empty_widget.dart';
+import 'package:blood_source/ui/shared/widgets/loading_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -23,6 +25,10 @@ class DonorView extends StatelessWidget {
       viewModelBuilder: () => DonorViewModel(),
       onModelReady: (model) async => await model.init(),
       builder: (context, model, Widget? child) {
+        if (model.isBusy) {
+          return const LoadingIndicator();
+        }
+
         return Scaffold(
           backgroundColor: Colors.white,
           appBar: AppBar(
@@ -30,113 +36,109 @@ class DonorView extends StatelessWidget {
             backgroundColor: Colors.white,
             elevation: 0,
             actions: [
-              !fromRequestView
-                  ? const SizedBox()
-                  : AppTextButton(
-                      onTap: () => model.addRequest(request!),
-                      text: 'Add Request',
-                      padding: EdgeInsets.only(right: 18.r),
-                      fontSize: 16.sp,
-                      color: AppColors.primaryDark,
-                    )
+              AppTextButton(
+                onTap: () => model.addRequest(request!),
+                text: 'Add Request',
+                padding: EdgeInsets.only(right: 18.r),
+                fontSize: 16.sp,
+                color: AppColors.primaryDark,
+              )
             ],
           ),
-          body: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Center(
-                  child: Container(
-                    height: 160.h,
-                    width: 107.w,
-                    padding: EdgeInsets.symmetric(vertical: 18.r),
-                    decoration: BoxDecoration(
-                      color: AppColors.primary.withOpacity(0.03),
-                      borderRadius: BorderRadius.circular(14.r),
-                      border: Border.all(
-                        color: AppColors.swatch.shade500,
-                        width: 2.w,
-                      ),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Stack(
-                          children: [
-                            Align(
-                              alignment: Alignment.center,
-                              child: SizedBox(
-                                height: 65.h,
-                                width: 46.w,
-                                child:
-                                    Image.asset('assets/images/red-blood.png'),
-                              ),
+          body: model.donors.isEmpty
+              ? const EmptyWidget()
+              : SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Container(
+                          height: 160.h,
+                          width: 107.w,
+                          padding: EdgeInsets.symmetric(vertical: 18.r),
+                          decoration: BoxDecoration(
+                            color: AppColors.primary.withOpacity(0.03),
+                            borderRadius: BorderRadius.circular(14.r),
+                            border: Border.all(
+                              color: AppColors.swatch.shade500,
+                              width: 2.w,
                             ),
-                            Positioned(
-                              left: 48.w,
-                              top: 4.h,
-                              child: Container(
-                                alignment: Alignment.center,
-                                height: 32.h,
-                                width: 32.h,
-                                decoration: BoxDecoration(
-                                  color: const Color(0xFF464A57),
-                                  shape: BoxShape.circle,
-                                  border: Border.all(color: Colors.white),
-                                ),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Stack(
+                                children: [
+                                  Align(
+                                    alignment: Alignment.center,
+                                    child: SizedBox(
+                                      height: 65.h,
+                                      width: 46.w,
+                                      child: Image.asset(
+                                          'assets/images/red-blood.png'),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    left: 48.w,
+                                    top: 4.h,
+                                    child: Container(
+                                      alignment: Alignment.center,
+                                      height: 32.h,
+                                      width: 32.h,
+                                      decoration: BoxDecoration(
+                                        color: const Color(0xFF464A57),
+                                        shape: BoxShape.circle,
+                                        border: Border.all(color: Colors.white),
+                                      ),
+                                      child: Text(
+                                        BloodGroup
+                                            .values[
+                                                model.user!.bloodGroup!.index]
+                                            .value
+                                            .name,
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 12.sp,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              6.verticalSpace,
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 20.r),
                                 child: Text(
-                                  fromRequestView
-                                      ? BloodGroup
-                                          .values[request!.bloodGroup.index]
-                                          .value
-                                          .name
-                                      : BloodGroup
-                                          .values[model.user!.bloodGroup!.index]
-                                          .value
-                                          .name,
+                                  'A Positive (A+)',
+                                  textAlign: TextAlign.center,
                                   style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 12.sp,
+                                    fontSize: 13.sp,
                                     fontWeight: FontWeight.w500,
+                                    height: 1.4.h,
                                   ),
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                        6.verticalSpace,
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 20.r),
-                          child: Text(
-                            'A Positive (A+)',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 13.sp,
-                              fontWeight: FontWeight.w500,
-                              height: 1.4.h,
-                            ),
+                            ],
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                      20.verticalSpace,
+                      // Text(model.getDonorCountString()),
+                      20.verticalSpace,
+                      ListView.builder(
+                        itemCount: model.donors.length,
+                        shrinkWrap: true,
+                        padding: EdgeInsets.symmetric(horizontal: 18.r),
+                        itemBuilder: (context, i) {
+                          final donor = model.donors[i];
+                          return DonorListItem(donor: donor);
+                        },
+                      ),
+                    ],
                   ),
                 ),
-                20.verticalSpace,
-                Text(model.getDonorCountString()),
-                20.verticalSpace,
-                ListView.builder(
-                  itemCount: model.donors.length,
-                  shrinkWrap: true,
-                  padding: EdgeInsets.symmetric(horizontal: 26.r),
-                  itemBuilder: (context, i) {
-                    final donor = model.donors[i];
-                    return DonorListItem(donor: donor);
-                  },
-                ),
-              ],
-            ),
-          ),
         );
       },
     );
