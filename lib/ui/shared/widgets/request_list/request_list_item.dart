1e0f3c07392083fc0f7146bf83e03732676c413b
David Michael II <knowdavidmichael@gmail.com>
2022-05-31 16:29:48 +0100

    fix: navigation action

--- lib/ui/shared/widgets/request_list/request_list_item.dart
+++ lib/ui/shared/widgets/request_list/request_list_item.dart
@@ -1,17 +1,19 @@
 import 'package:blood_source/common/app_colors.dart';
 import 'package:blood_source/models/request.dart';
+import 'package:blood_source/ui/views/request_list/request_list_view_model.dart';
 import 'package:blood_source/utils/date_formatter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:blood_source/models/blood_group.dart';
+import 'package:stacked/stacked.dart';
 
-class RequestListItem extends StatelessWidget {
+class RequestListItem extends ViewModelWidget<RequestListViewModel> {
   const RequestListItem({Key? key, required this.request}) : super(key: key);
 
   final Request request;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, RequestListViewModel viewModel) {
     return Container(
       alignment: Alignment.center,
       margin: EdgeInsets.symmetric(horizontal: 18.r, vertical: 12.r),
@@ -29,6 +31,7 @@ class RequestListItem extends StatelessWidget {
       ),
       child: ListTile(
         title: Text(request.bloodGroup.value.desc),
+        onTap: () => viewModel.goToDetails(request),
         isThreeLine: true,
         leading: CircleAvatar(
           radius: 25.w,
