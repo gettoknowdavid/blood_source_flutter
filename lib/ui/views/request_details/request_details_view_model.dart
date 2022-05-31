799a4b27af915ac2bc4202ec4ac1b18d4f6e94c5
David Michael II <knowdavidmichael@gmail.com>
2022-05-31 16:48:04 +0100

    feat: get user from request details vm

--- lib/ui/views/request_details/request_details_view_model.dart
+++ lib/ui/views/request_details/request_details_view_model.dart
@@ -1,5 +1,25 @@
+import 'package:blood_source/app/app.locator.dart';
+import 'package:blood_source/models/request.dart';
+import 'package:blood_source/services/store_service.dart';
 import 'package:stacked/stacked.dart';
+import 'package:blood_source/models/blood_source_user.dart';
 
-class RequestDetailsViewModel extends BaseViewModel {
-  Future<void> init() async {}
+class RequestDetailsViewModel extends BaseViewModel with ReactiveServiceMixin {
+  RequestDetailsViewModel() {
+    listenToReactiveValues([_user]);
+  }
+
+  final StoreService _storeService = locator<StoreService>();
+
+  final ReactiveValue<BloodSourceUser?> _user =
+      ReactiveValue<BloodSourceUser?>(null);
+  BloodSourceUser? get user => _user.value;
+
+  Future<void> init(Request req) async {
+    setBusy(true);
+    final result = await _storeService.getUser(req.user.uid);
+    setBusy(false);
+    _user.value = result!.bSUser;
+    print(user!.name);
+  }
 }
