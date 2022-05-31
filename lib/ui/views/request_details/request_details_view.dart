08fc3f7e66b85b8d1ae65ffbedb03e25b1de5dd9
David Michael II <knowdavidmichael@gmail.com>
2022-05-31 18:37:33 +0100

    feat: add map to request details vm

--- lib/ui/views/request_details/request_details_view.dart
+++ lib/ui/views/request_details/request_details_view.dart
@@ -1,8 +1,12 @@
 import 'package:blood_source/common/app_colors.dart';
+import 'package:blood_source/models/blood_source_user.dart';
 import 'package:blood_source/models/request.dart';
 import 'package:blood_source/ui/shared/widgets/app_back_button.dart';
+import 'package:blood_source/ui/shared/widgets/app_button.dart';
 import 'package:blood_source/ui/shared/widgets/loading_indicator.dart';
+import 'package:blood_source/ui/shared/widgets/profile/blood_group_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 import 'package:stacked/stacked.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -23,41 +27,115 @@ class RequestDetailsView extends StatelessWidget {
         }
 
         return Scaffold(
-          appBar: AppBar(
-            leading: const AppBackButton(color: AppColors.primary),
-            backgroundColor: Colors.white,
-            elevation: 0,
-          ),
           backgroundColor: Colors.white,
-          body: SingleChildScrollView(
-            padding: const EdgeInsets.fromLTRB(18, 8, 18, 32).r,
-            child: Center(
-              child: Column(
-                children: [
-                  CircleAvatar(
-                    radius: 0.18 * 1.sw,
-                    backgroundColor: AppColors.swatch.shade100,
-                    child: CircleAvatar(
-                      radius: 0.17 * 1.sw,
-                      foregroundColor: AppColors.primary,
-                      foregroundImage: NetworkImage(request.user.avatar),
+          body: Stack(
+            children: [
+              SizedBox(
+                height: 1.sh,
+                width: 1.sw,
+                child: OSMFlutter(
+                  controller: model.controller,
+                  trackMyPosition: false,
+                  initZoom: 17,
+                  minZoomLevel: 8,
+                  maxZoomLevel: 18,
+                  stepZoom: 1.0,
+                  userLocationMarker: UserLocationMaker(
+                    personMarker: MarkerIcon(
+                      icon: Icon(
+                        Icons.location_history_rounded,
+                        color: Colors.red,
+                        size: 48.sp,
+                      ),
+                    ),
+                    directionArrowMarker: MarkerIcon(
+                      icon: Icon(Icons.double_arrow, size: 48.sp),
                     ),
                   ),
-                  20.verticalSpace,
-                  Text(
-                    model.user!.name!,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 22.sp,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: AppBar(
+                  elevation: 0,
+                  backgroundColor: Colors.transparent,
+                  leading: Container(
+                    child: const AppBackButton(color: AppColors.primary),
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         );
       },
+    );
+  }
+}
+
+class _BodyWidget extends StatelessWidget {
+  const _BodyWidget({
+    Key? key,
+    required this.request,
+    required this.user,
+  }) : super(key: key);
+
+  final Request request;
+  final BloodSourceUser user;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.fromLTRB(18, 8, 18, 32).r,
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            CircleAvatar(
+              radius: 0.18 * 1.sw,
+              backgroundColor: AppColors.swatch.shade100,
+              child: CircleAvatar(
+                radius: 0.17 * 1.sw,
+                foregroundColor: AppColors.primary,
+                foregroundImage: NetworkImage(request.user.avatar),
+              ),
+            ),
+            20.verticalSpace,
+            Text(
+              user.name!,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 22.sp,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            10.verticalSpace,
+            Text(
+              user.city!,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16.sp),
+            ),
+            10.verticalSpace,
+            !request.showContactInfo
+                ? const SizedBox()
+                : Text(
+                    user.phone!,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 16.sp),
+                  ),
+            20.verticalSpace,
+            BloodGroupWidget(
+              bloodGroup: request.bloodGroup,
+              type: BGWidgetType.complex,
+            ),
+            30.verticalSpace,
+            AppButton(onTap: () {}, text: 'Donate Blood'),
+          ],
+        ),
+      ),
     );
   }
 }
