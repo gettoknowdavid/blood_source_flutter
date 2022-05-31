4e5cda12b11cae16b6e443b08d4111db02cad0a7
David Michael II <knowdavidmichael@gmail.com>
2022-05-31 20:31:17 +0100

    feat: add map initilization t request details vm

--- lib/ui/views/request_details/request_details_view_model.dart
+++ lib/ui/views/request_details/request_details_view_model.dart
@@ -1,8 +1,7 @@
-import 'dart:math';
-
 import 'package:blood_source/app/app.locator.dart';
 import 'package:blood_source/models/request.dart';
 import 'package:blood_source/services/store_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 import 'package:stacked/stacked.dart';
 import 'package:blood_source/models/blood_source_user.dart';
@@ -10,71 +9,56 @@ import 'package:blood_source/models/blood_source_user.dart';
 class RequestDetailsViewModel extends BaseViewModel
     with ReactiveServiceMixin, OSMMixinObserver {
   RequestDetailsViewModel() {
-    listenToReactiveValues([_user]);
+    listenToReactiveValues([_recipient, _user]);
   }
-  MapController controller = MapController();
+
+  late MapController controller;
 
   final StoreService _storeService = locator<StoreService>();
+
+  final ReactiveValue<BloodSourceUser?> _recipient =
+      ReactiveValue<BloodSourceUser?>(null);
+  BloodSourceUser? get recipient => _recipient.value;
 
   final ReactiveValue<BloodSourceUser?> _user =
       ReactiveValue<BloodSourceUser?>(null);
   BloodSourceUser? get user => _user.value;
 
+  bool get compatible => user!.bloodGroup! == recipient!.bloodGroup!;
+
   Future<void> init(Request req) async {
     setBusy(true);
-    final result = await _storeService.getUser(req.user.uid);
-    setBusy(false);
-    _user.value = result!.bSUser;
-
-    controller = MapController(
-      initMapWithUserPosition: false,
-      initPosition: GeoPoint(
-        latitude: user!.location!.latitude,
-        longitude: user!.location!.longitude,
-      ),
-      // areaLimit: BoundingBox(
-      //   east: 10.4922941,
-      //   north: 47.8084648,
-      //   south: 45.817995,
-      //   west: 5.9559113,
-      // ),
+    final _recipientResult = await _storeService.getUser(req.user.uid);
+    final _userResult = await _storeService.getUser(
+      FirebaseAuth.instance.currentUser!.uid,
     );
+
+    controller = MapController(initMapWithUserPosition: true);
 
     controller.addObserver(this);
 
-    await longUpdateStuff();
+    setBusy(false);
+
+    _recipient.value = _recipientResult!.bSUser;
+    _user.value = _userResult!.bSUser;
+
+    notifyListeners();
   }
 
-  Future longUpdateStuff() async {
-    // Sets busy to true before starting future and sets it to false after executing
-    // You can also pass in an object as the busy object. Otherwise it'll use the ViewModel
-    var result = await runBusyFuture(mapIsInitialized());
-    return result;
-  }
-
-  Future<void> mapIsInitialized() async {
-    await controller.setStaticPosition(
-      [
-        GeoPointWithOrientation(
-          latitude: user!.location!.latitude,
-          longitude: user!.location!.longitude,
-        ),
-      ],
-      "line 2",
-    );
-  }
-
-  @override
-  Future<void> mapIsReady(bool isReady) async {
+  Future<void> mapInit(bool isReady) async {
     if (isReady) {
-      await mapIsInitialized();
+      await controller.setStaticPosition(
+        [
+          GeoPointWithOrientation(
+            latitude: recipient!.location!.latitude,
+            longitude: recipient!.location!.longitude,
+          ),
+        ],
+        "line 2",
+      );
     }
   }
 
   @override
-  void dispose() {
-    //controller.listenerMapIsReady.removeListener(mapIsInitialized);
-    controller.dispose();
-    super.dispose();
-  }
+  Future<void> mapIsReady(bool isReady) async => await mapInit(isReady);
 }
