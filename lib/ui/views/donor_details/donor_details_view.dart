2833b0eca11d31ee23ec760ed5f6d26d4e5264e9
David Michael II <knowdavidmichael@gmail.com>
2022-05-31 20:47:13 +0100

    feat: add donor argument to donor details view

--- lib/ui/views/donor_details/donor_details_view.dart
+++ lib/ui/views/donor_details/donor_details_view.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:blood_source/models/blood_source_user.dart';
 
 import './donor_details_view_model.dart';
 
 class DonorDetailsView extends StatelessWidget {
-  const DonorDetailsView({Key? key}) : super(key: key);
+  const DonorDetailsView({Key? key, required this.donor}) : super(key: key);
+  final BloodSourceUser donor;
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +14,9 @@ class DonorDetailsView extends StatelessWidget {
       viewModelBuilder: () => DonorDetailsViewModel(),
       onModelReady: (model) async => await model.init(),
       builder: (context, model, Widget? child) {
-        return const Scaffold(
+        return Scaffold(
           body: Center(
-            child: Text('DonorDetailsView'),
+            child: Text(donor.uid!),
           ),
         );
       },
