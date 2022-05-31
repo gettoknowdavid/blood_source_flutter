4614ca3f2a62fbaa1e105ed7bc9803b4a7344acc
David Michael II <knowdavidmichael@gmail.com>
2022-05-31 20:44:23 +0100

    feat: add map controller to donor details vm

--- lib/ui/views/donor_details/donor_details_view_model.dart
+++ lib/ui/views/donor_details/donor_details_view_model.dart
@@ -1,6 +1,28 @@
-
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 import 'package:stacked/stacked.dart';
 
-class DonorDetailsViewModel extends BaseViewModel {
-  Future<void> init() async {}
+class DonorDetailsViewModel extends BaseViewModel with OSMMixinObserver {
+  late MapController controller;
+
+  Future<void> init() async {
+    controller = MapController(initMapWithUserPosition: true);
+    controller.addObserver(this);
+  }
+
+  Future<void> mapInit(bool isReady) async {
+    if (isReady) {
+      await controller.setStaticPosition(
+        [
+          // GeoPointWithOrientation(
+          //   latitude: recipient!.location!.latitude,
+          //   longitude: recipient!.location!.longitude,
+          // ),
+        ],
+        "line 2",
+      );
+    }
+  }
+
+  @override
+  Future<void> mapIsReady(bool isReady) async => await mapInit(isReady);
 }
