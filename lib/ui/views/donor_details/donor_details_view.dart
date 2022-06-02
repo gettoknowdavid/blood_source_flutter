78ee41091e088140001f462e9fe3e9ea24db1dde
David Michael II <knowdavidmichael@gmail.com>
2022-06-02 23:08:04 +0100

    feat: add body to donor details view

--- lib/ui/views/donor_details/donor_details_view.dart
+++ lib/ui/views/donor_details/donor_details_view.dart
@@ -1,6 +1,13 @@
+import 'package:blood_source/common/app_colors.dart';
+import 'package:blood_source/ui/shared/widgets/app_back_button.dart';
+import 'package:blood_source/ui/shared/widgets/loading_indicator.dart';
+import 'package:blood_source/ui/shared/widgets/request_details/map_panel_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:stacked/stacked.dart';
 import 'package:blood_source/models/blood_source_user.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import './donor_details_view_model.dart';
 
@@ -14,9 +21,56 @@ class DonorDetailsView extends StatelessWidget {
       viewModelBuilder: () => DonorDetailsViewModel(),
       onModelReady: (model) async => await model.init(donor),
       builder: (context, model, Widget? child) {
+        if (model.isBusy) {
+          return const LoadingIndicator();
+        }
+
         return Scaffold(
-          body: Center(
-            child: Text(donor.uid!),
+          backgroundColor: Colors.transparent,
+          extendBodyBehindAppBar: true,
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            leading: Container(
+              margin: EdgeInsets.only(left: 8.r, top: 8.r),
+              child: const AppBackButton(color: AppColors.primary),
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          body: Stack(
+            clipBehavior: Clip.none,
+            fit: StackFit.loose,
+            children: [
+              SlidingUpPanel(
+                body: OSMFlutter(
+                  controller: model.controller,
+                  trackMyPosition: false,
+                  initZoom: 17,
+                  minZoomLevel: 8,
+                  maxZoomLevel: 18,
+                  stepZoom: 1.0,
+                  userLocationMarker: UserLocationMaker(
+                    personMarker: MarkerIcon(
+                      icon: Icon(
+                        Icons.location_history_rounded,
+                        color: Colors.red,
+                        size: 48.sp,
+                      ),
+                    ),
+                    directionArrowMarker: MarkerIcon(
+                      icon: Icon(Icons.double_arrow, size: 48.sp),
+                    ),
+                  ),
+                ),
+                minHeight: 0.17.sh,
+                maxHeight: 0.54.sh,
+                borderRadius: BorderRadius.vertical(top: Radius.circular(30.r)),
+                // panelBuilder: (c) => MapPanel(controller: c, request: request),
+              ),
+            ],
           ),
         );
       },
