675b00b0eae96fbed44e78bc55ac48dabe4dc602
David Michael II <knowdavidmichael@gmail.com>
2022-06-02 23:04:51 +0100

    feat: add map config to donor details vm

--- lib/ui/views/donor_details/donor_details_view_model.dart
+++ lib/ui/views/donor_details/donor_details_view_model.dart
@@ -1,28 +1,43 @@
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:blood_source/models/blood_source_user.dart';
 import 'package:stacked/stacked.dart';
 
-class DonorDetailsViewModel extends BaseViewModel with OSMMixinObserver {
+class DonorDetailsViewModel extends BaseViewModel
+    with ReactiveServiceMixin, OSMMixinObserver {
+  DonorDetailsViewModel() {
+    listenToReactiveValues([_geoPoint]);
+  }
+
+  final ReactiveValue<GeoPointWithOrientation?> _geoPoint =
+      ReactiveValue<GeoPointWithOrientation?>(null);
+  GeoPointWithOrientation? get geoPoint => _geoPoint.value;
+
   late MapController controller;
 
-  Future<void> init() async {
+  bool isMapReady = false;
+
+  Future<void> init(BloodSourceUser donor) async {
+    setBusy(isMapReady);
+    _geoPoint.value = GeoPointWithOrientation(
+      latitude: donor.location!.latitude,
+      longitude: donor.location!.longitude,
+    );
     controller = MapController(initMapWithUserPosition: true);
     controller.addObserver(this);
+    setBusy(isMapReady);
+    notifyListeners();
   }
 
   Future<void> mapInit(bool isReady) async {
-    if (isReady) {
-      await controller.setStaticPosition(
-        [
-          // GeoPointWithOrientation(
-          //   latitude: recipient!.location!.latitude,
-          //   longitude: recipient!.location!.longitude,
-          // ),
-        ],
-        "line 2",
-      );
+    if (isReady && geoPoint != null) {
+      await controller.setStaticPosition([_geoPoint.value!], "line 2");
     }
   }
 
   @override
-  Future<void> mapIsReady(bool isReady) async => await mapInit(isReady);
+  Future<void> mapIsReady(bool isReady) async {
+    await mapInit(isReady);
+    isMapReady = true;
+    notifyListeners();
+  }
 }
