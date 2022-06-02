9291b02dd9915f963c5f06175af5a6ad5e049a89
David Michael II <knowdavidmichael@gmail.com>
2022-06-02 23:29:43 +0100

    feat: add recipient getter to donor details vm

--- lib/ui/views/donor_details/donor_details_view_model.dart
+++ lib/ui/views/donor_details/donor_details_view_model.dart
@@ -1,12 +1,19 @@
+import 'package:blood_source/app/app.locator.dart';
+import 'package:blood_source/services/store_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 import 'package:blood_source/models/blood_source_user.dart';
 import 'package:stacked/stacked.dart';
 
-class DonorDetailsViewModel extends BaseViewModel
+class DonorDetailsViewModel extends ReactiveViewModel
     with ReactiveServiceMixin, OSMMixinObserver {
   DonorDetailsViewModel() {
     listenToReactiveValues([_geoPoint]);
   }
+
+  final StoreService _storeService = locator<StoreService>();
+
+  BloodSourceUser get recipient => _storeService.bloodUser!;
 
   final ReactiveValue<GeoPointWithOrientation?> _geoPoint =
       ReactiveValue<GeoPointWithOrientation?>(null);
@@ -17,14 +24,17 @@ class DonorDetailsViewModel extends BaseViewModel
   bool isMapReady = false;
 
   Future<void> init(BloodSourceUser donor) async {
-    setBusy(isMapReady);
+    setBusy(true);
+
     _geoPoint.value = GeoPointWithOrientation(
       latitude: donor.location!.latitude,
       longitude: donor.location!.longitude,
     );
     controller = MapController(initMapWithUserPosition: true);
     controller.addObserver(this);
-    setBusy(isMapReady);
+    final uid = FirebaseAuth.instance.currentUser!.uid;
+    await _storeService.getUser(uid);
+    setBusy(false);
     notifyListeners();
   }
 
@@ -40,4 +50,7 @@ class DonorDetailsViewModel extends BaseViewModel
     isMapReady = true;
     notifyListeners();
   }
+
+  @override
+  List<ReactiveServiceMixin> get reactiveServices => [_storeService];
 }
