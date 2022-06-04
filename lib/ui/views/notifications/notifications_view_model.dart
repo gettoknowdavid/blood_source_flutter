e3c14aac3007f561ef4bd9f0b19fcce71087a916
David Michael II <knowdavidmichael@gmail.com>
2022-06-05 00:04:45 +0100

    update notifications vm

--- lib/ui/views/notifications/notifications_view_model.dart
+++ lib/ui/views/notifications/notifications_view_model.dart
@@ -2,16 +2,4 @@ import 'package:stacked/stacked.dart';
 
 class NotificationsViewModel extends BaseViewModel {
   Future<void> init() async {}
-
-  final ReactiveValue<List<Notification>?> _notifications =
-      ReactiveValue<List<Notification>?>([]);
-
-  List<Notification>? get notifications => _notifications.value;
-
-  Future longUpdateStuff() async {}
-}
-
-class Notification {
-  Notification(this.message);
-  final String message;
 }
