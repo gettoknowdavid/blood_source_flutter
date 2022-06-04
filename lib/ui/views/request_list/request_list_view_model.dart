585a2076b7d687a6bdd6aa1e51b79ed244362620
David Michael II <knowdavidmichael@gmail.com>
2022-06-05 00:07:24 +0100

    feat: switch to streamview model in request list vm

--- lib/ui/views/request_list/request_list_view_model.dart
+++ lib/ui/views/request_list/request_list_view_model.dart
@@ -2,24 +2,15 @@ import 'package:blood_source/app/app.locator.dart';
 import 'package:blood_source/app/app.router.dart';
 import 'package:blood_source/models/request.dart';
 import 'package:blood_source/services/store_service.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-class RequestListViewModel extends ReactiveViewModel with ReactiveServiceMixin {
-  RequestListViewModel() {
-    listenToReactiveValues([_compatible]);
-  }
-
+class RequestListViewModel extends StreamViewModel<QuerySnapshot<Request?>> {
   final NavigationService _navService = locator<NavigationService>();
   final StoreService _storeService = locator<StoreService>();
 
-  final ReactiveValue<List<Request>?> _requests =
-      ReactiveValue<List<Request>?>([]);
-  List<Request>? get requests => _requests.value;
-
-  final ReactiveValue<bool> _compatible = ReactiveValue<bool>(true);
-  bool get compatible => _compatible.value;
+  bool compatible = true;
 
   goToDetails(Request req) {
     _navService.navigateTo(
@@ -28,58 +19,19 @@ class RequestListViewModel extends ReactiveViewModel with ReactiveServiceMixin {
     );
   }
 
-  Future<void> init() async {
-    _compatible.listen((v) async {
-      if (v) {
-        setBusy(true);
-        await getCompatibleRequests();
+  Future<void> init() async {}
 
-        setBusy(false);
-      } else {
-        setBusy(true);
-        await getRequests();
-
-        setBusy(false);
-      }
-    });
-  }
-
-  Future longUpdateStuff() async {
-    var result = await runBusyFuture(getCompatibleRequests());
-    return result;
-  }
-
-  void Function(bool?)? onCompatibilityChanged(bool? value) {
-    _compatible.value = value!;
+  void onCompatibilityChanged() {
+    compatible = !compatible;
     notifyListeners();
-    return null;
-  }
-
-  Future<List<Request>?> getCompatibleRequests() async {
-    final uid = FirebaseAuth.instance.currentUser!.uid;
-    final _storeResult = await _storeService.getUser(uid);
-    final _user = _storeResult!.bSUser!;
-    print(_user.bloodGroup!);
-    final _r = await _storeService.getCompatibleRequests(_user.bloodGroup!);
-    if (_r.requests != null) {
-      _requests.value = _r.requests;
-      return _requests.value;
-    } else {
-      _requests.value = [];
-      return _requests.value;
-    }
-  }
-
-  Future<List<Request>?> getRequests() async {
-    final result = await _storeService.getRequests();
-    if (result.isRequestsEmpty) {
-      return [];
-    } else {
-      _requests.value = result.requests!;
-      return _requests.value;
-    }
+    notifySourceChanged();
   }
 
   @override
   List<ReactiveServiceMixin> get reactiveServices => [_storeService];
+
+  @override
+  Stream<QuerySnapshot<Request?>> get stream => compatible
+      ? _storeService.getCompatibleRequests()
+      : _storeService.getRequests();
 }
