ed0f474f926cab63d69092728d384370be3913a9
David Michael II <knowdavidmichael@gmail.com>
2022-06-05 10:04:04 +0100

    feat: add profiel details list args

--- lib/ui/shared/widgets/profile/profile_details_list.dart
+++ lib/ui/shared/widgets/profile/profile_details_list.dart
@@ -1,62 +1,58 @@
 import 'package:blood_source/common/app_icons.dart';
 import 'package:blood_source/models/gender.dart';
 import 'package:blood_source/ui/shared/widgets/profile/profile_details_item.dart';
-import 'package:blood_source/ui/views/profile/profile_view_model.dart';
+import 'package:blood_source/models/blood_source_user.dart';
 import 'package:flutter/material.dart';
-import 'package:stacked/stacked.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class ProfileDetailsList extends ViewModelWidget<ProfileViewModel> {
-  const ProfileDetailsList({Key? key}) : super(key: key);
+class ProfileDetailsList extends StatelessWidget {
+  const ProfileDetailsList({Key? key, required this.user}) : super(key: key);
+  final BloodSourceUser? user;
 
   @override
-  Widget build(BuildContext context, ProfileViewModel viewModel) {
-    if (viewModel.isBusy) {
-      return const Scaffold(body: Center(child: CircularProgressIndicator()));
-    } else {
-      return ListView(
-        shrinkWrap: true,
-        primary: false,
-        children: [
-          Title(
-            color: Colors.black,
-            child: Text(
-              'Other Details',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w500),
-            ),
+  Widget build(BuildContext context) {
+    return ListView(
+      shrinkWrap: true,
+      primary: false,
+      children: [
+        Title(
+          color: Colors.black,
+          child: Text(
+            'Other Details',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w500),
           ),
-          ProfileDetailsItem(
-            icon: AppIcons.gender,
-            title: 'Gender',
-            value: Gender.values[viewModel.user.gender!.index].value,
-          ),
-          Divider(height: 1.h, color: Colors.black26),
-          ProfileDetailsItem(
-            icon: AppIcons.age,
-            title: 'Age',
-            value: viewModel.user.age.toString(),
-          ),
-          Divider(height: 1.h, color: Colors.black26),
-          ProfileDetailsItem(
-            icon: AppIcons.height,
-            title: 'Height',
-            value: viewModel.user.height.toString(),
-          ),
-          Divider(height: 1.h, color: Colors.black26),
-          ProfileDetailsItem(
-            icon: AppIcons.weight,
-            title: 'Weight',
-            value: viewModel.user.weight.toString(),
-          ),
-          Divider(height: 1.h, color: Colors.black26),
-          ProfileDetailsItem(
-            icon: const Icon(Icons.location_city),
-            title: 'City',
-            value: viewModel.user.city,
-          ),
-        ],
-      );
-    }
+        ),
+        ProfileDetailsItem(
+          icon: AppIcons.gender,
+          title: 'Gender',
+          value: user!.gender!.value,
+        ),
+        Divider(height: 1.h, color: Colors.black26),
+        ProfileDetailsItem(
+          icon: AppIcons.age,
+          title: 'Age',
+          value: user!.age.toString(),
+        ),
+        Divider(height: 1.h, color: Colors.black26),
+        ProfileDetailsItem(
+          icon: AppIcons.height,
+          title: 'Height',
+          value: user!.height.toString(),
+        ),
+        Divider(height: 1.h, color: Colors.black26),
+        ProfileDetailsItem(
+          icon: AppIcons.weight,
+          title: 'Weight',
+          value: user!.weight.toString(),
+        ),
+        Divider(height: 1.h, color: Colors.black26),
+        ProfileDetailsItem(
+          icon: const Icon(Icons.location_city),
+          title: 'City',
+          value: user!.city,
+        ),
+      ],
+    );
   }
 }
