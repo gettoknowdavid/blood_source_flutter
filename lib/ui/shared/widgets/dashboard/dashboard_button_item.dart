a9e4f0406906e16ceaed2d80294f38f734e96729
David Michael II <knowdavidmichael@gmail.com>
2022-06-05 03:36:40 +0100

    feat: update dashboard button item

--- lib/ui/shared/widgets/dashboard/dashboard_button_item.dart
+++ lib/ui/shared/widgets/dashboard/dashboard_button_item.dart
@@ -1,20 +1,25 @@
 import 'package:blood_source/app/app.locator.dart';
+import 'package:blood_source/app/app.router.dart';
 import 'package:blood_source/models/dashboard_button_model.dart';
+import 'package:blood_source/ui/views/dashboard/dashboard_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-class DashboardButtonItem extends StatelessWidget {
+class DashboardButtonItem extends ViewModelWidget<DashboardViewModel> {
   const DashboardButtonItem({Key? key, required this.model}) : super(key: key);
 
   final DashboardButtonModel model;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, DashboardViewModel viewModel) {
     final NavigationService _navService = locator<NavigationService>();
 
     return GestureDetector(
-      onTap: () => _navService.navigateTo(model.route),
+      onTap: model.route == Routes.donorView
+          ? () => viewModel.goToDonors()
+          : () => _navService.navigateTo(model.route),
       child: Container(
         alignment: Alignment.center,
         height: 0.5 * 1.sh,
@@ -42,9 +47,13 @@ class DashboardButtonItem extends StatelessWidget {
               ),
             ),
             12.verticalSpace,
-            Text(
-              model.title,
-              style: TextStyle(fontSize: 16.sp),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0.r),
+              child: Text(
+                model.title,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16.sp),
+              ),
             ),
             15.verticalSpace,
             Container(
