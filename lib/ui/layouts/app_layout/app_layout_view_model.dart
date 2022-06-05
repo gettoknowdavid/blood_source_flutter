69bd19f7c2e0ce8eceb6482f6cd7b721ca58ccfc
David Michael II <knowdavidmichael@gmail.com>
2022-06-06 00:17:34 +0100

    feat: add eventCounts to app layout

--- lib/ui/layouts/app_layout/app_layout_view_model.dart
+++ lib/ui/layouts/app_layout/app_layout_view_model.dart
@@ -1,6 +1,7 @@
 import 'package:blood_source/app/app.locator.dart';
 import 'package:blood_source/app/app.router.dart';
 import 'package:blood_source/models/user-type.dart';
+import 'package:blood_source/services/event_service.dart';
 import 'package:blood_source/services/store_service.dart';
 import 'package:blood_source/ui/shared/widgets/loading_indicator.dart';
 import 'package:blood_source/ui/views/dashboard/dashboard_view.dart';
@@ -14,8 +15,16 @@ import 'package:stacked_services/stacked_services.dart';
 
 class AppLayoutViewModel extends IndexTrackingViewModel
     with ReactiveServiceMixin {
+  AppLayoutViewModel() {
+    listenToReactiveValues([_eventsCount]);
+  }
+
   final NavigationService _navService = locator<NavigationService>();
   final StoreService _storeService = locator<StoreService>();
+  final EventService _eventService = locator<EventService>();
+
+  final ReactiveValue<int> _eventsCount = ReactiveValue<int>(0);
+  int get eventsCount => _eventsCount.value;
 
   UserType get userType => _storeService.bloodUser!.userType;
 
@@ -37,6 +46,7 @@ class AppLayoutViewModel extends IndexTrackingViewModel
   }
 
   Future<void> init() async {
+    _eventsCount.value = _eventService.getEventsCount();
     await longUpdateStuff();
   }
 
