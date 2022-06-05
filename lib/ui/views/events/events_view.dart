ec74acf113b686029e5005697f2463910c31fc46
David Michael II <knowdavidmichael@gmail.com>
2022-06-05 18:54:39 +0100

    feat: add create event button to EventsView

--- lib/ui/views/events/events_view.dart
+++ lib/ui/views/events/events_view.dart
@@ -1,5 +1,8 @@
+import 'package:blood_source/common/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:stacked/stacked.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import './events_view_model.dart';
 
@@ -20,11 +23,41 @@ class EventsView extends StatelessWidget {
       ) {
         return Scaffold(
           appBar: AppBar(title: const Text('Events')),
+          floatingActionButton: Container(
+            decoration: BoxDecoration(
+              color: AppColors.primary,
+              borderRadius: BorderRadius.circular(16.r),
+              border: Border.all(color: AppColors.primary),
+            ),
+            child: IconButton(
+              onPressed: model.openBottomSheet,
+              color: Colors.white,
+              icon: const Icon(PhosphorIcons.plusBold),
+            ),
+          ),
           body: SingleChildScrollView(
             child: Center(
               child: Column(
-                children: const [
-                  Text('EventsView'),
+                children: [
+                  20.verticalSpace,
+                  const Text('You have 2 new events'),
+                  20.verticalSpace,
+                  Container(
+                    height: 0.14.sh,
+                    width: 1.sw,
+                    margin: const EdgeInsets.fromLTRB(18, 0, 18, 18).r,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16.r),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 20.r,
+                            color: Colors.black12,
+                            offset: Offset(0.w, 20.h),
+                          )
+                        ]),
+                  ),
                 ],
               ),
             ),
