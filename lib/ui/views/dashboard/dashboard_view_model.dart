9fc8795c110adc2c83e19c4ff0a8eb6892fc2468
David Michael II <knowdavidmichael@gmail.com>
2022-06-05 15:10:58 +0100

    feat: add donor and request count to dashboard button

--- lib/ui/views/dashboard/dashboard_view_model.dart
+++ lib/ui/views/dashboard/dashboard_view_model.dart
@@ -1,5 +1,6 @@
 import 'package:blood_source/app/app.locator.dart';
 import 'package:blood_source/app/app.router.dart';
+import 'package:blood_source/common/app_colors.dart';
 import 'package:blood_source/models/dashboard_button_model.dart';
 import 'package:blood_source/models/request.dart';
 import 'package:blood_source/models/request_user.dart';
@@ -8,6 +9,7 @@ import 'package:blood_source/models/user_location.dart';
 import 'package:blood_source/services/auth_service.dart';
 import 'package:blood_source/services/store_service.dart';
 import 'package:blood_source/models/blood_source_user.dart';
+import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:uuid/uuid.dart';
@@ -28,6 +30,10 @@ class DashboardViewModel extends ReactiveViewModel with ReactiveServiceMixin {
   final StoreService _storeService = locator<StoreService>();
   final NavigationService _navService = locator<NavigationService>();
 
+  // final ReactiveValue<int> _donorCount = ReactiveValue<int>(0);
+  int get donorCount => _storeService.donorCount;
+  int get requestCount => _storeService.requestCount;
+
   final ReactiveValue<List<DashboardButtonModel>> _buttonList =
       ReactiveValue<List<DashboardButtonModel>>(donorButtonList);
   List<DashboardButtonModel> get buttonList => _buttonList.value;
@@ -41,13 +47,13 @@ class DashboardViewModel extends ReactiveViewModel with ReactiveServiceMixin {
   Future<List<DashboardButtonModel>> getList() async {
     switch (user.userType) {
       case UserType.donor:
-        _buttonList.value = donorButtonList;
+        _buttonList.value = await getDonorButtonList();
         break;
       case UserType.recipient:
-        _buttonList.value = recipientButtonList;
+        _buttonList.value = await getRecipientButtonList();
         break;
       default:
-        _buttonList.value = donorButtonList;
+        _buttonList.value = await getDonorButtonList();
     }
     return _buttonList.value;
   }
@@ -76,6 +82,96 @@ class DashboardViewModel extends ReactiveViewModel with ReactiveServiceMixin {
       Routes.donorView,
       arguments: DonorViewArguments(request: request, fromRequestView: false),
     );
+  }
+
+  Future<List<DashboardButtonModel>> getDonorButtonList() async {
+    await _storeService.getDonorCount();
+    await _storeService.getMyRequestCount();
+
+    return <DashboardButtonModel>[
+      DashboardButtonModel(
+        'Find Requests',
+        '$donorCount',
+        const Icon(Icons.search, color: AppColors.primary),
+        const Color(0xFFFA6393),
+        Routes.requestListView,
+      ),
+      DashboardButtonModel(
+        'My Requests',
+        '$requestCount',
+        const ImageIcon(
+          AssetImage('assets/images/bell.png'),
+          color: AppColors.primary,
+        ),
+        const Color(0xFFFA6393),
+        Routes.myRequestsListView,
+      ),
+      DashboardButtonModel(
+        'Events',
+        'Map',
+        const ImageIcon(
+          AssetImage('assets/images/blood.png'),
+          color: Color(0xFF00CC99),
+        ),
+        const Color(0xFF00CC99),
+        Routes.donateView,
+      ),
+      DashboardButtonModel(
+        'Others',
+        'More',
+        const ImageIcon(
+          AssetImage('assets/images/settings.png'),
+          color: Color(0xFF999999),
+        ),
+        const Color(0xFF999999),
+        Routes.donateView,
+      ),
+    ];
+  }
+
+  Future<List<DashboardButtonModel>> getRecipientButtonList() async {
+    await _storeService.getDonorCount();
+    await _storeService.getMyRequestCount();
+
+    return <DashboardButtonModel>[
+      DashboardButtonModel(
+        'Find a Donor',
+        '$donorCount',
+        const Icon(Icons.search, color: AppColors.primary),
+        const Color(0xFFFA6393),
+        Routes.donorView,
+      ),
+      DashboardButtonModel(
+        'My Requests',
+        '$requestCount',
+        const ImageIcon(
+          AssetImage('assets/images/bell.png'),
+          color: AppColors.primary,
+        ),
+        const Color(0xFFFA6393),
+        Routes.myRequestsListView,
+      ),
+      DashboardButtonModel(
+        'Events',
+        'Map',
+        const ImageIcon(
+          AssetImage('assets/images/blood.png'),
+          color: Color(0xFF00CC99),
+        ),
+        const Color(0xFF00CC99),
+        Routes.donateView,
+      ),
+      DashboardButtonModel(
+        'Others',
+        'More',
+        const ImageIcon(
+          AssetImage('assets/images/settings.png'),
+          color: Color(0xFF999999),
+        ),
+        const Color(0xFF999999),
+        Routes.donateView,
+      ),
+    ];
   }
 
   @override
