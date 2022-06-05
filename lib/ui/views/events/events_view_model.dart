698ed553c4089c1f438568f29a28660c2fe4969e
David Michael II <knowdavidmichael@gmail.com>
2022-06-05 18:53:25 +0100

    feat: add textControllers and bottomSheet launcher

--- lib/ui/views/events/events_view_model.dart
+++ lib/ui/views/events/events_view_model.dart
@@ -1,5 +1,29 @@
+import 'package:blood_source/app/app.locator.dart';
+import 'package:blood_source/models/user_location.dart';
+import 'package:blood_source/utils/bottom_sheet_type.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 class EventsViewModel extends BaseViewModel {
   Future<void> init() async {}
+
+  final NavigationService _navService = locator<NavigationService>();
+  final BottomSheetService _bottomSheetService = locator<BottomSheetService>();
+
+  void openBottomSheet() =>
+      _bottomSheetService.showCustomSheet(variant: BottomSheetType.createEvent);
+
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  TextEditingController locationController = TextEditingController();
+  DateTime? timeAdded;
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    descriptionController.dispose();
+    locationController.dispose();
+    super.dispose();
+  }
 }
