4c2385361f72e432231d1bc0b69c2d121217b467
David Michael II <knowdavidmichael@gmail.com>
2022-06-05 06:50:56 +0100

    feat: add grid view to donate view

--- lib/ui/views/donate/donate_view.dart
+++ lib/ui/views/donate/donate_view.dart
@@ -1,27 +1,50 @@
+import 'package:blood_source/ui/shared/widgets/app_back_button.dart';
+import 'package:blood_source/ui/shared/widgets/donate/contact_button_item.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:blood_source/models/blood_source_user.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import './donate_view_model.dart';
 
 class DonateView extends StatelessWidget {
-  const DonateView({Key? key}) : super(key: key);
+  const DonateView({Key? key, required this.donor}) : super(key: key);
+  final BloodSourceUser donor;
 
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<DonateViewModel>.reactive(
       viewModelBuilder: () => DonateViewModel(),
-      onModelReady: (DonateViewModel model) async {
-        await model.init();
-      },
-      builder: (
-        BuildContext context,
-        DonateViewModel model,
-        Widget? child,
-      ) {
+      onModelReady: (DonateViewModel model) async => await model.init(),
+      builder: (context, model, Widget? child) {
         return Scaffold(
+          appBar: AppBar(leading: const AppBackButton()),
           body: Center(
-            child: Text(
-              'DonateView',
+            child: SizedBox(
+              height: 0.6 * 1.sh,
+              child: GridView.builder(
+                primary: false,
+                padding: EdgeInsets.symmetric(
+                  horizontal: 32.w,
+                  vertical: 30.h,
+                ),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 9 / 11,
+                  crossAxisSpacing: 20.h,
+                  mainAxisSpacing: 20.h,
+                ),
+                itemCount: model.buttons.length,
+                itemBuilder: (BuildContext context, i) {
+                  return ContactButtonItem(
+                    model: model.buttons[i],
+                    onTap: () => model.getAction(
+                      model.buttons[i].contactType,
+                      donor,
+                    ),
+                  );
+                },
+              ),
             ),
           ),
         );
