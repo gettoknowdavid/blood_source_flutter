b2da3bb8056566c199969e5e60f04a1726a45d86
David Michael II <knowdavidmichael@gmail.com>
2022-06-05 18:44:17 +0100

    chore: add generated event creator

---
+++ lib/models/event_creator.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'event_creator.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+EventCreator _$EventCreatorFromJson(Map<String, dynamic> json) => EventCreator(
+      uid: json['uid'] as String,
+      name: json['name'] as String,
+      avatar: json['avatar'] as String,
+    );
+
+Map<String, dynamic> _$EventCreatorToJson(EventCreator instance) =>
+    <String, dynamic>{
+      'uid': instance.uid,
+      'name': instance.name,
+      'avatar': instance.avatar,
+    };
