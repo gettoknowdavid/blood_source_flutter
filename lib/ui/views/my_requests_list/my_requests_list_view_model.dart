f98efc826566ede216934736b95ca7dc74ee5d92
David Michael II <knowdavidmichael@gmail.com>
2022-06-06 01:12:30 +0100

    feat: updae my requests view

--- lib/ui/views/my_requests_list/my_requests_list_view_model.dart
+++ lib/ui/views/my_requests_list/my_requests_list_view_model.dart
@@ -1,36 +1,34 @@
 import 'package:blood_source/app/app.locator.dart';
 import 'package:blood_source/models/request.dart';
-import 'package:blood_source/services/store_service.dart';
+import 'package:blood_source/services/request_service.dart';
 import 'package:stacked/stacked.dart';
 
-class MyRequestsListViewModel extends ReactiveViewModel {
-  final StoreService _storeService = locator<StoreService>();
-
-  // List<Request>? get requests => _storeService.requests;
+class MyRequestsListViewModel extends ReactiveViewModel
+    with ReactiveServiceMixin {
+  MyRequestsListViewModel() {
+    listenToReactiveValues([_requests]);
+  }
+  final RequestService _requestService = locator<RequestService>();
 
   final ReactiveValue<List<Request>?> _requests =
       ReactiveValue<List<Request>?>([]);
   List<Request>? get myRequests => _requests.value;
 
   Future<void> init() async {
-    await longUpdateStuff();
+    setBusy(true);
+    await getMyRequests();
+    setBusy(false);
   }
 
-  Future longUpdateStuff() async {
-    var result = await runBusyFuture(getRequests());
-    return result;
-  }
-
-  Future<List<Request>?> getRequests() async {
-    final result = await _storeService.getMyRequests();
+  Future getMyRequests() async {
+    final result = await _requestService.getMyRequests();
     if (result.isRequestsEmpty) {
-      return [];
+      _requests.value = [];
     } else {
       _requests.value = result.requests!;
-      return _requests.value;
     }
   }
 
   @override
-  List<ReactiveServiceMixin> get reactiveServices => [_storeService];
+  List<ReactiveServiceMixin> get reactiveServices => [_requestService];
 }
