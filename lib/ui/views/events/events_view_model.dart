409c3ee9745e506e99e4848469d57afe19137dac
David Michael II <knowdavidmichael@gmail.com>
2022-06-13 19:02:39 +0100

    fix: lint errors in events view

--- lib/ui/views/events/events_view_model.dart
+++ lib/ui/views/events/events_view_model.dart
@@ -2,19 +2,14 @@ import 'dart:async';
 
 import 'package:blood_source/app/app.locator.dart';
 import 'package:blood_source/models/event.dart';
-import 'package:blood_source/models/event_creator.dart';
 import 'package:blood_source/services/event_service.dart';
-import 'package:blood_source/services/store_service.dart';
 import 'package:blood_source/ui/shared/setup_snack_bar_ui.dart';
 import 'package:blood_source/utils/bottom_sheet_type.dart';
 import 'package:blood_source/utils/date_formatter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
-import 'package:intl/intl.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
-import 'package:uuid/uuid.dart';
 
 class EventsViewModel extends FutureViewModel<EventResult>
     with ReactiveServiceMixin {
@@ -37,7 +32,6 @@ class EventsViewModel extends FutureViewModel<EventResult>
 
   late StreamSubscription<InternetConnectionStatus> subscription;
 
-  final StoreService _storeService = locator<StoreService>();
   final EventService _eventService = locator<EventService>();
   final BottomSheetService _bottomSheetService = locator<BottomSheetService>();
   final SnackbarService _snackbarService = locator<SnackbarService>();
@@ -48,61 +42,19 @@ class EventsViewModel extends FutureViewModel<EventResult>
   final ReactiveValue<bool> _verified = ReactiveValue<bool>(false);
   bool get verified => _verified.value;
 
-  TextEditingController titleController = TextEditingController();
-  TextEditingController descriptionController = TextEditingController();
-  TextEditingController locationController = TextEditingController();
-  TextEditingController dateController = TextEditingController();
-  TextEditingController timeController = TextEditingController();
-
   bool? isConnected;
 
   DateTime? timeAdded;
 
   void openBottomSheet() {
-    _bottomSheetService.showCustomSheet(variant: BottomSheetType.createEvent);
-  }
-
-  bool formVerified() {
-    if (titleController.text.isNotEmpty &&
-        descriptionController.text.isNotEmpty &&
-        locationController.text.isNotEmpty &&
-        dateController.text.isNotEmpty &&
-        timeController.text.isNotEmpty) {
-      notifyListeners();
-      return true;
-    } else {
-      notifyListeners();
-      return false;
-    }
-  }
-
-  void setEventDate(context) async {
-    DateTime? pickedDate = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2101),
-    );
-
-    if (pickedDate != null) {
-      final formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate);
-      dateController.text = formattedDate;
-      notifyListeners();
-    }
-  }
-
-  void setEventTime(context) async {
-    TimeOfDay? pickedTime = await showTimePicker(
-      context: context,
-      initialTime: TimeOfDay.now(),
-      initialEntryMode: TimePickerEntryMode.input,
-    );
-
-    if (pickedTime != null) {
-      String formattedTime = pickedTime.format(context);
-      timeController.text = formattedTime;
-      notifyListeners();
-    }
+    _bottomSheetService
+        .showCustomSheet(variant: BottomSheetType.createEvent)
+        .then((value) {
+      if (value!.data != null) {
+        _eventService.createNewEvent(value.data);
+        initialise();
+      }
+    });
   }
 
   bool isEventBelongToUser(Event ev) {
@@ -121,35 +73,6 @@ class EventsViewModel extends FutureViewModel<EventResult>
       return 'You created this event on ${dateFormatter(ev.timeAdded.toIso8601String())}.';
     } else {
       return '${ev.creator.name} created this event on ${dateFormatter(ev.timeAdded.toIso8601String())}.';
-    }
-  }
-
-  Future createNewEvent() async {
-    if (formVerified()) {
-      final _uid = FirebaseAuth.instance.currentUser!.uid;
-      final _r = await _storeService.getUser(_uid);
-
-      await _eventService.createNewEvent(Event(
-        uid: const Uuid().v4(),
-        title: titleController.text,
-        description: descriptionController.text,
-        location: locationController.text,
-        date: DateTime.parse(dateController.text),
-        time: timeController.text,
-        timeAdded: DateTime.now(),
-        creator: EventCreator(
-          uid: _uid,
-          name: _r!.bSUser!.name!,
-          avatar: _r.bSUser!.avatar!,
-        ),
-      ));
-      notifyListeners();
-      clearFields();
-    } else {
-      _snackbarService.showCustomSnackBar(
-        variant: SnackbarType.negative,
-        message: 'All items are required. Please fill them in.',
-      );
     }
   }
 
@@ -215,19 +138,8 @@ class EventsViewModel extends FutureViewModel<EventResult>
 
   @override
   void dispose() {
-    titleController.dispose();
-    descriptionController.dispose();
-    locationController.dispose();
     subscription.cancel();
     super.dispose();
-  }
-
-  void clearFields() {
-    titleController.clear();
-    descriptionController.clear();
-    locationController.clear();
-    dateController.clear();
-    timeController.clear();
   }
 
   @override
