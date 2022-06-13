95040f7e0c1137b6d5164a139c591bf65cdffe42
David Michael II <knowdavidmichael@gmail.com>
2022-06-13 12:41:28 +0100

    feat: add init edit store key

--- lib/common/storage_keys.dart
+++ lib/common/storage_keys.dart
@@ -1,3 +1,5 @@
 class StorageKeys {
   static const String bloodSourceUser = '_blood_source_user_';
+  static const String initScreen = '_init_screen_';
+  static const String initEdit = '_init_edit_';
 }
