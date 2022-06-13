bd36142152fa5f674313fda11b870ccce796ea6c
David Michael II <knowdavidmichael@gmail.com>
2022-06-13 20:34:36 +0100

    fix: compatible donors getter

--- lib/utils/compatible_donors.dart
+++ lib/utils/compatible_donors.dart
@@ -4,8 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:blood_source/models/blood_source_user.dart';
 
-Stream<QuerySnapshot<BloodSourceUser?>> compatibleDonors(
-    BloodGroup bloodGroup) {
+Future<List<BloodSourceUser>> compatibleDonors(BloodGroup bloodGroup) {
   final ref = FirebaseFirestore.instance
       .collection('users')
       .withConverter<BloodSourceUser>(
@@ -14,84 +13,109 @@ Stream<QuerySnapshot<BloodSourceUser?>> compatibleDonors(
       );
 
   final uid = FirebaseAuth.instance.currentUser!.uid;
+
   switch (bloodGroup) {
     case BloodGroup.aPositive:
       return ref
           .where('uid', isNotEqualTo: uid)
           .where('userType', isEqualTo: UserType.donor.name)
           .where('bloodGroup', whereIn: [
-        BloodGroup.aPositive.value.desc,
-        BloodGroup.aNegative.value.desc,
-        BloodGroup.oPositive.value.desc,
-        BloodGroup.oNegative.value.desc,
-      ]).snapshots();
+            BloodGroup.aPositive.value.desc,
+            BloodGroup.aNegative.value.desc,
+            BloodGroup.oPositive.value.desc,
+            BloodGroup.oNegative.value.desc,
+          ])
+          .get()
+          .then((snap) => snap.docs.map((e) => e.data()).toList())
+          .timeout(const Duration(seconds: 10));
 
     case BloodGroup.oPositive:
       return ref
           .where('uid', isNotEqualTo: uid)
           .where('userType', isEqualTo: UserType.donor.name)
           .where('bloodGroup', whereIn: [
-        BloodGroup.oPositive.value.desc,
-        BloodGroup.oNegative.value.desc,
-      ]).snapshots();
+            BloodGroup.oPositive.value.desc,
+            BloodGroup.oNegative.value.desc,
+          ])
+          .get()
+          .then((snap) => snap.docs.map((e) => e.data()).toList())
+          .timeout(const Duration(seconds: 10));
 
     case BloodGroup.bPositive:
       return ref
           .where('uid', isNotEqualTo: uid)
           .where('userType', isEqualTo: UserType.donor.name)
           .where('bloodGroup', whereIn: [
-        BloodGroup.bPositive.value.desc,
-        BloodGroup.bNegative.value.desc,
-        BloodGroup.oPositive.value.desc,
-        BloodGroup.oNegative.value.desc,
-      ]).snapshots();
+            BloodGroup.bPositive.value.desc,
+            BloodGroup.bNegative.value.desc,
+            BloodGroup.oPositive.value.desc,
+            BloodGroup.oNegative.value.desc,
+          ])
+          .get()
+          .then((snap) => snap.docs.map((e) => e.data()).toList())
+          .timeout(const Duration(seconds: 10));
 
     case BloodGroup.abPositive:
       return ref
           .where('uid', isNotEqualTo: uid)
           .where('userType', isEqualTo: UserType.donor.name)
-          .snapshots();
+          .get()
+          .then((snap) => snap.docs.map((e) => e.data()).toList())
+          .timeout(const Duration(seconds: 10));
 
     case BloodGroup.aNegative:
       return ref
           .where('uid', isNotEqualTo: uid)
           .where('userType', isEqualTo: UserType.donor.name)
           .where('bloodGroup', whereIn: [
-        BloodGroup.aNegative.value.desc,
-        BloodGroup.oNegative.value.desc,
-      ]).snapshots();
+            BloodGroup.aNegative.value.desc,
+            BloodGroup.oNegative.value.desc,
+          ])
+          .get()
+          .then((snap) => snap.docs.map((e) => e.data()).toList())
+          .timeout(const Duration(seconds: 10));
 
     case BloodGroup.oNegative:
       return ref
           .where('uid', isNotEqualTo: uid)
           .where('userType', isEqualTo: UserType.donor.name)
           .where('bloodGroup', isEqualTo: BloodGroup.oNegative.value.desc)
-          .snapshots();
+          .get()
+          .then((snap) => snap.docs.map((e) => e.data()).toList())
+          .timeout(const Duration(seconds: 10));
 
     case BloodGroup.bNegative:
       return ref
           .where('uid', isNotEqualTo: uid)
           .where('userType', isEqualTo: UserType.donor.name)
           .where('bloodGroup', whereIn: [
-        BloodGroup.bNegative.value.desc,
-        BloodGroup.oNegative.value.desc,
-      ]).snapshots();
+            BloodGroup.bNegative.value.desc,
+            BloodGroup.oNegative.value.desc,
+          ])
+          .get()
+          .then((snap) => snap.docs.map((e) => e.data()).toList())
+          .timeout(const Duration(seconds: 10));
 
     case BloodGroup.abNegative:
       return ref
           .where('uid', isNotEqualTo: uid)
           .where('userType', isEqualTo: UserType.donor.name)
           .where('bloodGroup', whereIn: [
-        BloodGroup.abNegative.value.desc,
-        BloodGroup.aNegative.value.desc,
-        BloodGroup.bNegative.value.desc,
-        BloodGroup.oNegative.value.desc,
-      ]).snapshots();
+            BloodGroup.abNegative.value.desc,
+            BloodGroup.aNegative.value.desc,
+            BloodGroup.bNegative.value.desc,
+            BloodGroup.oNegative.value.desc,
+          ])
+          .get()
+          .then((snap) => snap.docs.map((e) => e.data()).toList())
+          .timeout(const Duration(seconds: 10));
 
     default:
       return ref
           .where('uid', isNotEqualTo: uid)
           .where('userType', isEqualTo: UserType.donor.name)
-          .snapshots();
+          .get()
+          .then((snap) => snap.docs.map((e) => e.data()).toList())
+          .timeout(const Duration(seconds: 10));
   }
 }
