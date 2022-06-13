ca5bbf0388f7c21cd54d27cd1af5fb4f43f1c885
David Michael II <knowdavidmichael@gmail.com>
2022-06-13 12:58:01 +0100

    feat: add connection checker to profie view model

--- lib/ui/views/profile/profile_view_model.dart
+++ lib/ui/views/profile/profile_view_model.dart
@@ -5,17 +5,61 @@ import 'package:blood_source/app/app.router.dart';
 import 'package:blood_source/models/blood_source_user.dart';
 import 'package:blood_source/services/auth_service.dart';
 import 'package:blood_source/services/store_service.dart';
+import 'package:blood_source/ui/shared/setup_snack_bar_ui.dart';
 import 'package:blood_source/ui/views/edit_profile/edit_profile_view.dart';
 import 'package:blood_source/utils/dialog_type.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class ProfileViewModel extends ReactiveViewModel {
+  ProfileViewModel() {
+    subscription = InternetConnectionChecker().onStatusChange.listen((status) {
+      switch (status) {
+        case InternetConnectionStatus.connected:
+          isConnected = true;
+          notifyListeners();
+          break;
+        case InternetConnectionStatus.disconnected:
+          isConnected = false;
+          notifyListeners();
+          break;
+      }
+    });
+  }
+
+  late StreamSubscription<InternetConnectionStatus> subscription;
+
   final DialogService _dialogService = locator<DialogService>();
   final NavigationService _navService = locator<NavigationService>();
   final AuthService _authService = locator<AuthService>();
   final StoreService _storeService = locator<StoreService>();
+  final SnackbarService _snackbarService = locator<SnackbarService>();
+
+  bool? isConnected;
+
+  Future<void> checkConnectivity() async {
+    bool isConn = await InternetConnectionChecker().hasConnection;
+    switch (isConn) {
+      case true:
+        _snackbarService.showCustomSnackBar(
+          message: 'Yay! You\'re connected!',
+          variant: SnackbarType.positive,
+          duration: const Duration(seconds: 3),
+        );
+        break;
+      case false:
+        _snackbarService.showCustomSnackBar(
+          message: 'We are convinced you\'re disconnected. Try again.',
+          variant: SnackbarType.negative,
+          duration: const Duration(seconds: 3),
+        );
+        break;
+      default:
+        null;
+    }
+  }
 
   Future<void> init() async {
     await longUpdateStuff();
@@ -26,6 +70,8 @@ class ProfileViewModel extends ReactiveViewModel {
   Future longUpdateStuff() async {
     // Sets busy to true before starting future and sets it to false after executing
     // You can also pass in an object as the busy object. Otherwise it'll use the ViewModel
+    isConnected = await InternetConnectionChecker().hasConnection;
+    notifyListeners();
     var result = await runBusyFuture(getProfile());
     return result;
   }
@@ -52,4 +98,10 @@ class ProfileViewModel extends ReactiveViewModel {
 
   @override
   List<ReactiveServiceMixin> get reactiveServices => [_storeService];
+
+  @override
+  void dispose() {
+    subscription.cancel();
+    super.dispose();
+  }
 }
