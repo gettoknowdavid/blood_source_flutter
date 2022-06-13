fc07980ed45795006712acf0e185805b191a7857
David Michael II <knowdavidmichael@gmail.com>
2022-06-13 13:00:23 +0100

    feat: add offline widget to request list view

--- lib/ui/views/request_list/request_list_view.dart
+++ lib/ui/views/request_list/request_list_view.dart
@@ -2,6 +2,7 @@ import 'package:blood_source/ui/shared/widgets/app_back_button.dart';
 import 'package:blood_source/ui/shared/widgets/app_text_button.dart';
 import 'package:blood_source/ui/shared/widgets/empty_widget.dart';
 import 'package:blood_source/ui/shared/widgets/loading_indicator.dart';
+import 'package:blood_source/ui/shared/widgets/offline_widget.dart';
 import 'package:blood_source/ui/shared/widgets/request_list/request_list_item.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
@@ -18,7 +19,7 @@ class RequestListView extends StatelessWidget {
       viewModelBuilder: () => RequestListViewModel(),
       onModelReady: (model) async => await model.init(),
       builder: (context, model, Widget? child) {
-        if (!model.dataReady || model.isBusy) {
+        if (!model.dataReady || model.isBusy || model.isConnected == null) {
           return const LoadingIndicator();
         }
 
@@ -28,33 +29,40 @@ class RequestListView extends StatelessWidget {
             leading: const AppBackButton(),
             elevation: 0,
             actions: [
-              AppTextButton(
-                onTap: model.onCompatibilityChanged,
-                text: model.compatible ? 'Show All' : 'Show Compatible',
-                fontSize: 16.sp,
-                color: Colors.white,
-                padding: EdgeInsets.only(right: 18.r),
-              )
+              !model.isConnected!
+                  ? const SizedBox()
+                  : AppTextButton(
+                      onTap: model.onCompatibilityChanged,
+                      text: model.compatible ? 'Show All' : 'Show Compatible',
+                      fontSize: 16.sp,
+                      color: Colors.white,
+                      padding: EdgeInsets.only(right: 18.r),
+                    )
             ],
           ),
-          body: model.data!.docs.isEmpty
-              ? const EmptyWidget(message: 'There are currently no requests.')
-              : SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      20.verticalSpace,
-                      ListView.builder(
-                        primary: false,
-                        shrinkWrap: true,
-                        itemCount: model.data!.docs.length,
-                        itemBuilder: (context, i) {
-                          final data = model.data!.docs[i].data()!;
+          body: !model.isConnected!
+              ? OfflineWidget(onTap: model.checkConnectivity, addPadding: true)
+              : Container(
+                  child: model.data!.docs.isEmpty
+                      ? const EmptyWidget(
+                          message: 'There are currently no requests.')
+                      : SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              20.verticalSpace,
+                              ListView.builder(
+                                primary: false,
+                                shrinkWrap: true,
+                                itemCount: model.data!.docs.length,
+                                itemBuilder: (context, i) {
+                                  final data = model.data!.docs[i].data()!;
 
-                          return RequestListItem(request: data);
-                        },
-                      ),
-                    ],
-                  ),
+                                  return RequestListItem(request: data);
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
                 ),
         );
       },
