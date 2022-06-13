180ff106a3a51401dbb93765fc5c151aca426915
David Michael II <knowdavidmichael@gmail.com>
2022-06-13 12:55:59 +0100

    feat: add offline widget to forgot password view

--- lib/ui/views/forgot_password/forgot_password_view.dart
+++ lib/ui/views/forgot_password/forgot_password_view.dart
@@ -3,6 +3,8 @@ import 'package:blood_source/ui/layouts/auth_layout.dart';
 import 'package:blood_source/ui/shared/widgets/app_button.dart';
 import 'package:blood_source/ui/shared/widgets/app_textfield.dart';
 import 'package:blood_source/ui/shared/widgets/auth_layout_header.dart';
+import 'package:blood_source/ui/shared/widgets/loading_indicator.dart';
+import 'package:blood_source/ui/shared/widgets/offline_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -18,58 +20,66 @@ class ForgotPasswordView extends StatelessWidget {
       viewModelBuilder: () => ForgotPasswordViewModel(),
       onModelReady: (model) async => await model.init(),
       builder: (context, model, Widget? child) {
+        if (model.isBusy || model.isConnected == null) {
+          return const LoadingIndicator();
+        }
+
         return AuthLayout(
           showBackButton: true,
-          child: Form(
-            key: model.forgotPasswordForm,
-            child: SingleChildScrollView(
-              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  const AuthLayoutHeader(
-                    title: 'Forgot Password?',
-                    subtitle:
-                        "Don't worry! It happens. \nWe just need the address linked with your account.",
-                  ),
-                  20.verticalSpace,
-                  Image.asset(ImageResources.forgotPassword),
-                  20.verticalSpace,
-                  AppTextField(
-                    controller: model.emailController,
-                    keyboardType: TextInputType.emailAddress,
-                    hintText: 'Enter email',
-                    onChanged: (value) => model.onChanged(value),
-                    textInputAction: TextInputAction.done,
-                  ),
-                  20.verticalSpace,
-                  model.signInError == null
-                      ? const SizedBox()
-                      : Column(
-                          children: [
-                            Text(
-                              model.signInError!,
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: 9.sp,
-                                fontStyle: FontStyle.italic,
-                                fontWeight: FontWeight.w600,
-                                color: Theme.of(context).errorColor,
-                              ),
-                            ),
-                            20.verticalSpace,
-                          ],
+          child: !model.isConnected!
+              ? OfflineWidget(onTap: model.checkConnectivity)
+              : Form(
+                  key: model.forgotPasswordForm,
+                  child: SingleChildScrollView(
+                    keyboardDismissBehavior:
+                        ScrollViewKeyboardDismissBehavior.onDrag,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const AuthLayoutHeader(
+                          title: 'Forgot Password?',
+                          subtitle:
+                              "Don't worry! It happens. \nWe just need the address linked with your account.",
                         ),
-                  AppButton(
-                    onTap:
-                        model.isFormValidated() ? () => model.submit() : null,
-                    text: 'Submit',
+                        20.verticalSpace,
+                        Image.asset(ImageResources.forgotPassword),
+                        20.verticalSpace,
+                        AppTextField(
+                          controller: model.emailController,
+                          keyboardType: TextInputType.emailAddress,
+                          hintText: 'Enter email',
+                          onChanged: (value) => model.onChanged(value),
+                          textInputAction: TextInputAction.done,
+                        ),
+                        20.verticalSpace,
+                        model.signInError == null
+                            ? const SizedBox()
+                            : Column(
+                                children: [
+                                  Text(
+                                    model.signInError!,
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 9.sp,
+                                      fontStyle: FontStyle.italic,
+                                      fontWeight: FontWeight.w600,
+                                      color: Theme.of(context).errorColor,
+                                    ),
+                                  ),
+                                  20.verticalSpace,
+                                ],
+                              ),
+                        AppButton(
+                          onTap: model.isFormValidated()
+                              ? () => model.submit()
+                              : null,
+                          text: 'Submit',
+                        ),
+                      ],
+                    ),
                   ),
-                ],
-              ),
-            ),
-          ),
+                ),
         );
       },
     );
