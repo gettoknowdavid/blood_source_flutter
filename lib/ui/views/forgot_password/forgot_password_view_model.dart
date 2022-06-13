ca57fc54f5e2f917799f9448f0a56da69c7d72b2
David Michael II <knowdavidmichael@gmail.com>
2022-06-13 12:55:41 +0100

    feat; add connection checker to forgot password vm

--- lib/ui/views/forgot_password/forgot_password_view_model.dart
+++ lib/ui/views/forgot_password/forgot_password_view_model.dart
@@ -1,17 +1,38 @@
+import 'dart:async';
+
 import 'package:blood_source/app/app.locator.dart';
 import 'package:blood_source/app/app.router.dart';
+import 'package:blood_source/ui/shared/setup_snack_bar_ui.dart';
 import 'package:blood_source/utils/dialog_type.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_firebase_auth/stacked_firebase_auth.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-class ForgotPasswordViewModel extends BaseViewModel with ReactiveServiceMixin {
-  Future<void> init() async {}
+class ForgotPasswordViewModel extends BaseViewModel {
+  ForgotPasswordViewModel() {
+    subscription = InternetConnectionChecker().onStatusChange.listen((status) {
+      switch (status) {
+        case InternetConnectionStatus.connected:
+          isConnected = true;
+          notifyListeners();
+          break;
+        case InternetConnectionStatus.disconnected:
+          isConnected = false;
+          notifyListeners();
+          break;
+      }
+    });
+  }
 
-  DialogService dialogService = locator<DialogService>();
-  NavigationService navigationService = locator<NavigationService>();
+  late StreamSubscription<InternetConnectionStatus> subscription;
+
+  final DialogService _dialogService = locator<DialogService>();
+  final NavigationService _navigationService = locator<NavigationService>();
+  final SnackbarService _snackbarService = locator<SnackbarService>();
+
   FirebaseAuthenticationService authService =
       locator<FirebaseAuthenticationService>();
 
@@ -19,6 +40,30 @@ class ForgotPasswordViewModel extends BaseViewModel with ReactiveServiceMixin {
   GlobalKey<FormState> get forgotPasswordForm => _forgotPasswordForm;
 
   final TextEditingController emailController = TextEditingController();
+
+  bool? isConnected;
+
+  Future<void> checkConnectivity() async {
+    bool isConn = await InternetConnectionChecker().hasConnection;
+    switch (isConn) {
+      case true:
+        _snackbarService.showCustomSnackBar(
+          message: 'Yay! You\'re connected!',
+          variant: SnackbarType.positive,
+          duration: const Duration(seconds: 3),
+        );
+        break;
+      case false:
+        _snackbarService.showCustomSnackBar(
+          message: 'We are convinced you\'re disconnected. Try again.',
+          variant: SnackbarType.negative,
+          duration: const Duration(seconds: 3),
+        );
+        break;
+      default:
+        null;
+    }
+  }
 
   bool isFormValidated() {
     if (emailController.text.isEmpty ||
@@ -38,13 +83,13 @@ class ForgotPasswordViewModel extends BaseViewModel with ReactiveServiceMixin {
 
   Future submit() async {
     if (_forgotPasswordForm.currentState!.validate()) {
-      dialogService.showCustomDialog(variant: DialogType.loading);
+      _dialogService.showCustomDialog(variant: DialogType.loading);
       try {
         await FirebaseAuth.instance.sendPasswordResetEmail(
           email: emailController.text.trim(),
         );
-        navigationService.popRepeated(1);
-        navigationService.navigateTo(Routes.checkEmailView);
+        _navigationService.popRepeated(1);
+        _navigationService.navigateTo(Routes.checkEmailView);
       } on FirebaseAuthException catch (e) {
         if (e.code == 'user-not-found') {
           signInError = 'Oops! We have no record of this user';
@@ -55,13 +100,15 @@ class ForgotPasswordViewModel extends BaseViewModel with ReactiveServiceMixin {
     notifyListeners();
   }
 
-  void initialise() {
+  Future<void> init() async {
+    isConnected = await InternetConnectionChecker().hasConnection;
     notifyListeners();
   }
 
   @override
   void dispose() {
     emailController.dispose();
+    subscription.cancel();
     super.dispose();
   }
 }
