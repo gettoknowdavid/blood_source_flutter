d2aa5e4ed6da9bfc370b8f531e68a5432afebcdf
David Michael II <knowdavidmichael@gmail.com>
2022-06-13 12:56:57 +0100

    feat: add offline widgeet to my request list view

--- lib/ui/views/my_requests_list/my_requests_list_view.dart
+++ lib/ui/views/my_requests_list/my_requests_list_view.dart
@@ -1,9 +1,13 @@
 import 'package:blood_source/models/blood_group.dart';
+import 'package:blood_source/models/request.dart';
 import 'package:blood_source/ui/shared/widgets/app_back_button.dart';
 import 'package:blood_source/ui/shared/widgets/empty_widget.dart';
 import 'package:blood_source/ui/shared/widgets/loading_indicator.dart';
+import 'package:blood_source/ui/shared/widgets/offline_widget.dart';
 import 'package:blood_source/utils/date_formatter.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:stacked/stacked.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -18,7 +22,7 @@ class MyRequestsListView extends StatelessWidget {
       viewModelBuilder: () => MyRequestsListViewModel(),
       onModelReady: (model) async => await model.init(),
       builder: (context, model, Widget? child) {
-        if (model.isBusy) {
+        if (model.isBusy || !model.dataReady || model.isConnected == null) {
           return const LoadingIndicator();
         }
 
@@ -28,59 +32,81 @@ class MyRequestsListView extends StatelessWidget {
             leading: const AppBackButton(),
             elevation: 0,
           ),
-          body: model.myRequests!.isEmpty
-              ? const EmptyWidget(
-                  message: 'It seems you haven\'t made any requests yet.',
-                )
-              : SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      20.verticalSpace,
-                      ListView.builder(
-                        primary: false,
-                        shrinkWrap: true,
-                        itemCount: model.myRequests!.length,
-                        itemBuilder: (context, i) {
-                          final request = model.myRequests![i];
-                          BloodGroup bloodGroup = request.bloodGroup;
+          body: !model.isConnected!
+              ? OfflineWidget(onTap: model.checkConnectivity, addPadding: true)
+              : Container(
+                  child: model.data!.docs.isEmpty
+                      ? const EmptyWidget(
+                          message:
+                              'It seems you haven\'t made any requests yet.',
+                        )
+                      : SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              20.verticalSpace,
+                              ListView.builder(
+                                primary: false,
+                                shrinkWrap: true,
+                                itemCount: model.data!.docs.length,
+                                itemBuilder: (context, i) {
+                                  List<QueryDocumentSnapshot<Request>>
+                                      requests = model.data!.docs;
+                                  final request = requests[i].data();
 
-                          return ListTile(
-                            title: Text(bloodGroup.value.desc),
-                            isThreeLine: true,
-                            subtitle: Text(
-                              dateFormatter(
-                                request.timeAdded!.toIso8601String(),
+                                  return _MyRequestsListItem(
+                                    request: request,
+                                    onDelete: () {
+                                      requests.removeAt(i);
+                                      model.notifyListeners();
+                                      model.deleteRequest(request.uid);
+                                    },
+                                  );
+                                },
                               ),
-                              style: const TextStyle(
-                                fontStyle: FontStyle.italic,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                            trailing: Container(
-                              padding: EdgeInsets.all(7.r),
-                              decoration: BoxDecoration(
-                                color: request.requestGranted
-                                    ? Colors.green
-                                    : Colors.red,
-                                borderRadius: BorderRadius.circular(8.r),
-                              ),
-                              child: Text(
-                                request.requestGranted ? 'GRANTED' : 'PENDING',
-                                style: TextStyle(
-                                  fontSize: 10.sp,
-                                  fontWeight: FontWeight.w600,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
+                            ],
+                          ),
+                        ),
                 ),
         );
       },
+    );
+  }
+}
+
+class _MyRequestsListItem extends StatelessWidget {
+  const _MyRequestsListItem({
+    Key? key,
+    required this.request,
+    required this.onDelete,
+  }) : super(key: key);
+
+  final Request request;
+  final void Function()? onDelete;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 8.r),
+      child: ListTile(
+        title: Text(request.bloodGroup.value.desc),
+        isThreeLine: true,
+        subtitle: Text(
+          dateFormatter(
+            request.timeAdded!.toIso8601String(),
+          ),
+          style: const TextStyle(
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        trailing: IconButton(
+          onPressed: onDelete,
+          icon: const Icon(
+            PhosphorIcons.trash,
+            color: Colors.red,
+          ),
+        ),
+      ),
     );
   }
 }
