c10cc81e4da203cab9ef28b837ae4e0a85ef32f8
David Michael II <knowdavidmichael@gmail.com>
2022-06-14 16:01:52 +0100

    fix: remove avatar from event creator model

--- lib/models/event_creator.dart
+++ lib/models/event_creator.dart
@@ -5,25 +5,19 @@ part 'event_creator.g.dart';
 
 @JsonSerializable(explicitToJson: true)
 class EventCreator {
-  EventCreator({required this.uid, required this.name, required this.avatar});
+  EventCreator({required this.uid, required this.name});
 
   final String uid;
   final String name;
-  final String avatar;
 
   EventCreator.fromFirestore(
     DocumentSnapshot<Map<String, dynamic>> snapshot,
     SnapshotOptions? options,
   )   : uid = snapshot.data()?['uid'] as String,
-        name = snapshot.data()?['name'] as String,
-        avatar = snapshot.data()?['avatar'] as String;
+        name = snapshot.data()?['name'] as String;
 
   Map<String, dynamic> toFirestore() {
-    return {
-      "uid": uid,
-      "name": name,
-      "avatar": avatar,
-    };
+    return {"uid": uid, "name": name};
   }
 
   factory EventCreator.fromJson(Map<String, dynamic> json) =>
